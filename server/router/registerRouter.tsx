6aa2c54986e008223ccb5cdc4cb20e232fb2ab09
qirtudgus <qirtudgus@naver.com>
2022-07-21 02:54:36 +0900

    modules/createHashPassword.tsx 패스워드 암호화 모듈 분리 구현 회원가입 api 함수가 다수의 기능을 가지는것을 최소화하기위해 패스워드 암호화 작업 함수를 따로 생성하였다. string타입의 패스워드를 인자로 호출하면 salt값과 hashPassowrd가 담긴 객체를 반환해준다

--- server/router/registerRouter.tsx
+++ server/router/registerRouter.tsx
@@ -1,16 +1,17 @@
 import express, { Request, Response, NextFunction } from 'express';
 import CryptoJS from 'crypto-js';
 import {db} from '../db.js';
+import {createHashPassword} from "../../src/modules/createHashPassword.js"
 
 
 export const registerRouter = express.Router();
 
 registerRouter.post('/join', (req:Request, res: Response, next:NextFunction) => {
     const {id,password} = req.body;
-    const salt :string = CryptoJS.lib.WordArray.random(128 / 8).toString(CryptoJS.enc.Hex);
-    const hashPassword :string = CryptoJS.HmacSHA256(password, salt).toString();
+    const a =  createHashPassword(password)
     const sqlQuery = 'INSERT INTO users (Id,Password,Salt) VALUES (?,?,?)';
-    db.query(sqlQuery, [id, hashPassword, salt]);
+    db.query(sqlQuery, [id, a.hashPassword, a.salt]);
+
     console.log(`${id}님 회원가입 완료`);
     res.send("200")
 })
