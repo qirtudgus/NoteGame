51978d5c1167aa8f1464f5a9a680cfa4d2f8c9aa
qirtudgus <qirtudgus@naver.com>
2022-08-02 06:22:39 +0900

    골드가 0 이하로 내려가지않도록 구현 deduct api에서 리워드 연산 결과가 음수일 경우 0을 응답해주게했다

--- server/router/pengameRouter.tsx
+++ server/router/pengameRouter.tsx
@@ -3,25 +3,21 @@ import { db } from '../db.js';
 
 export const pengameRouter = express.Router();
 
+const userFindQuery = 'SELECT Gold FROM users WHERE ID = ?';
+const rewardUpdateQuery = `UPDATE users SET Gold = ? WHERE ID = ?`;
+const loginQuery = 'SELECT * FROM users WHERE ID = ?';
+
+
 pengameRouter.post('/multiple', (req, res, next) => {
   const userId = req.decoded.userId;
-  console.log(req.body)
-  console.log(req.body.reward)
-  console.log(req.body.speed)
   const reward = req.body.reward;
   const speed = req.body.speed;
-  console.log('곱하기 요청');
 
-  const userFindQuery = 'SELECT Gold FROM users WHERE ID = ?';
-  const multipleQuery = `UPDATE users SET Gold = ? WHERE ID = ?`;
-  const loginQuery = 'SELECT * FROM users WHERE ID = ?';
 
   db.query(userFindQuery, [userId], (err, result, fields) => {
     let resultGold = parseInt(result[0].Gold) * parseInt(reward);
-    console.log(result[0].Gold);
-    console.log(reward);
-    console.log(resultGold);
-    db.query(multipleQuery, [resultGold, userId], (err, result, fields) => {
+    console.log(`${userId}님께서 ${result[0].Gold}에서  ${resultGold}가 되었습니다.`)
+    db.query(rewardUpdateQuery, [resultGold, userId], (err, result, fields) => {
       db.query(loginQuery, [userId], function (err, rows, fields) {
         const userInfo = {
           Level: rows[0].Level,
@@ -42,21 +38,12 @@ pengameRouter.post('/add', (req, res, next) => {
   const userId = req.decoded.userId;
   const reward = req.body.reward;
   const speed = req.body.speed;
-  console.log(req.body)
-  console.log(req.body.reward)
-  console.log(req.body.speed)
-
-  const userFindQuery = 'SELECT Gold FROM users WHERE ID = ?';
-  const multipleQuery = `UPDATE users SET Gold = ? WHERE ID = ?`;
-  const loginQuery = 'SELECT * FROM users WHERE ID = ?';
 
   db.query(userFindQuery, [userId], (err, result, fields) => {
     let resultGold = parseInt(result[0].Gold) + (parseInt(reward) * speed);
 
-    console.log(result[0].Gold);
-    console.log(resultGold);
-
-    db.query(multipleQuery, [resultGold, userId], (err, result, fields) => {
+    console.log(`${userId}님께서 ${result[0].Gold}에서  ${resultGold}가 되었습니다.`)
+    db.query(rewardUpdateQuery, [resultGold, userId], (err, result, fields) => {
       db.query(loginQuery, [userId], function (err, rows, fields) {
         const userInfo = {
           Level: rows[0].Level,
@@ -78,21 +65,17 @@ pengameRouter.post('/deduct', (req, res, next) => {
   const reward = req.body.reward;
   const speed = req.body.speed;
 
-  console.log(req.body)
-  console.log(req.body.reward)
-  console.log(req.body.speed)
-
-  const userFindQuery = 'SELECT Gold FROM users WHERE ID = ?';
-  const multipleQuery = `UPDATE users SET Gold = ? WHERE ID = ?`;
-  const loginQuery = 'SELECT * FROM users WHERE ID = ?';
-
   db.query(userFindQuery, [userId], (err, result, fields) => {
     let resultGold = parseInt(result[0].Gold) -(parseInt(reward) * speed);
+    console.log(`${userId}님께서 ${result[0].Gold}에서  ${resultGold}가 되었습니다.`)
+    console.log(Math.sign(resultGold))
+    //음수 방지
+    if(Math.sign(resultGold) === -1){
+      resultGold = 0
+    }
 
-    console.log(result[0].Gold);
-    console.log(resultGold);
+    db.query(rewardUpdateQuery, [resultGold, userId], (err, result, fields) => {
 
-    db.query(multipleQuery, [resultGold, userId], (err, result, fields) => {
       db.query(loginQuery, [userId], function (err, rows, fields) {
         const userInfo = {
           Level: rows[0].Level,
