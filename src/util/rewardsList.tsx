cbcd8c0f40d9b26c85508bd2b76ea84fb371242a
qirtudgus <qirtudgus@naver.com>
2022-08-02 02:47:25 +0900

    리워드지급 switch문제거하고 구현! 리워드리스트를 객체로 바꿔 데이터를 더 유연하게 사용할 수 있게되었다.

--- src/util/rewardsList.tsx
+++ src/util/rewardsList.tsx
@@ -10,52 +10,66 @@ export const rewardsList: any = [
   '골드 5배 획득',
 ];
 
+const frontText:string = "골드"
+const deductText:string = "차감"
+const addText:string = "획득"
+const multipleText:string = "배"
 export const rewardsListObj: any = [
   {
-    front:"골드",
+    front:frontText,
     number:100,
-    back:"차감"
+    back:deductText,
+    action:"deduct"
   },
   {
-    front:"골드",
-    number:100,
-    back:"차감"
+    front:frontText,
+    number:200,
+    back:deductText,
+    action:"deduct"
   },
   {
-    front:"골드",
-    number:100,
-    back:"차감"
+    front:frontText,
+    number:500,
+    back:deductText,
+    action:"deduct"
   },
   {
-    front:"골드",
+    front:frontText,
     number:100,
-    back:"차감"
+    back:addText,
+    action:"add"
   },
   {
-    front:"골드",
-    number:100,
-    back:"차감"
+    front:frontText,
+    number:200,
+    back:addText,
+    action:"add"
   },
   {
-    front:"골드",
-    number:100,
-    back:"차감"
+    front:frontText,
+    number:500,
+    back:addText,
+    action:"add"
   },
   {
-    front:"골드",
-    number:100,
-    back:"차감"
+    front:frontText,
+    number:2,
+    back:multipleText,
+    action:"multiple"
   },
   {
-    front:"골드",
-    number:100,
-    back:"차감"
+    front:frontText,
+    number:3,
+    back:multipleText,
+    action:"multiple"
   },
   {
-    front:"골드",
-    number:100,
-    back:"차감"
+    front:frontText,
+    number:4,
+    back:multipleText,
+    action:"multiple"
   },
-
+  
+  
 ];
 
