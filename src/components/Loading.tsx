fea3a66dd009988e435f004d96d27326c23bfeee
qirtudgus <qirtudgus@naver.com>
2022-08-04 08:27:04 +0900

    볼펜돌리기 로딩스피너 구현 좌표값을 획득하는 0.5초동안 로딩스피너 출력하여 ui 개선 로딩스피너를 그냥 출력하면 요소 좌표가 로딩스피너를 반환하기때문에 포인터이벤트 none을 줘서 해결

---
+++ src/components/Loading.tsx
@@ -0,0 +1,96 @@
+import styled, { css, keyframes } from 'styled-components';
+
+const lds_ellipsis1 = keyframes`
+  0% {
+      transform: scale(0);
+    }
+    100% {
+      transform: scale(1);
+    }
+  `;
+
+const lds_ellipsis3 = keyframes`
+  0% {
+      transform: scale(1);
+    }
+    100% {
+      transform: scale(0);
+    }
+  `;
+
+const lds_ellipsis2 = keyframes`
+  0% {
+      transform: translate(0, 0);
+    }
+    100% {
+      transform: translate(24px, 0);
+    }
+  `;
+
+const IdsEllipsis = styled.div`
+  z-index: 1000;
+  display: inline-block;
+  position: relative;
+  width: 80px;
+  height: 80px;
+
+  & > div {
+    position: absolute;
+    top: 33px;
+    width: 13px;
+    height: 13px;
+    border-radius: 50%;
+    background: #fff;
+    animation-timing-function: cubic-bezier(0, 1, 1, 0);
+  }
+
+  & div:nth-child(1) {
+    left: 8px;
+    animation: ${lds_ellipsis1} 0.6s infinite;
+  }
+
+  & div:nth-child(2) {
+    left: 8px;
+    animation: ${lds_ellipsis2} 0.6s infinite;
+  }
+  & div:nth-child(3) {
+    left: 32px;
+    animation: ${lds_ellipsis2} 0.6s infinite;
+  }
+  & div:nth-child(4) {
+    left: 56px;
+    animation: ${lds_ellipsis3} 0.6s infinite;
+  }
+`;
+
+const LoadingWrap = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height 100%;
+  z-index: 1000;
+  background: rgba(0, 0, 0, 0.5);
+  display:flex;
+  justify-content:center;
+  align-items: center;
+  pointer-events: none; 
+
+`;
+
+const Loading = (props: any) => {
+  return (
+    <>
+      <LoadingWrap>
+        <IdsEllipsis>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </IdsEllipsis>
+      </LoadingWrap>
+    </>
+  );
+};
+
+export default Loading;
--- src/page/PlayPenGame.tsx
+++ src/page/PlayPenGame.tsx
@@ -1,9 +1,9 @@
 import styled, { keyframes, css } from 'styled-components';
 import UserInfo from '../components/userInfo';
-import React ,{ useCallback, useRef, useState } from 'react';
+import React, { useCallback, useRef, useState } from 'react';
 import BasicButtons from '../components/BasicBtn';
 import { pengame_request } from '../modules/login';
-import { useDispatch , useSelector  } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '../modules/modules_index';
 import { createRandomRewardsArray } from '../util/createRandomRewardsArray';
 import HomeBtn from '../components/HomeBtn';
@@ -12,6 +12,7 @@ import BackHistoryBtn from '../components/BackHistoryBtn';
 import RefreshBtn from '../components/RefreshBtn';
 import ResultModal from '../components/ResultModal';
 import ResultFalseModal from '../components/ResultFalseModal';
+import Loading from '../components/Loading';
 interface penAni {
   penStatus?: boolean;
   ref?: any;
@@ -80,8 +81,8 @@ const BoxWrap = styled.div`
   display: flex;
 `;
 
-interface BoxColor{
-  red?:string;
+interface BoxColor {
+  red?: string;
 }
 
 const Box = styled.div<BoxColor>`
@@ -103,11 +104,12 @@ const Box = styled.div<BoxColor>`
   &:last-child {
     border-right: 1px solid#000;
   }
-  ${(props) => props.red && css`
-  background: #e5005a;
-  color:#fff;
-
-  `}
+  ${(props) =>
+    props.red &&
+    css`
+      background: #e5005a;
+      color: #fff;
+    `}
 `;
 
 // document.addEventListener('click', logKey);
@@ -124,10 +126,11 @@ const PlayPenGame = () => {
   const [penStatus, setPenSatus] = useState<boolean>(true);
   const [refresh, setrefresh] = useState<boolean>(true);
   const [isModal, setIsModal] = useState<boolean>(false);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
   const [isFalseModal, setIsFalseModal] = useState<boolean>(false);
-  const [penSpeed, setPenSpeed] = useState<{speed:number,text:number}>({
-    speed : 1,
-    text : 1,
+  const [penSpeed, setPenSpeed] = useState<{ speed: number; text: number }>({
+    speed: 1,
+    text: 1,
   });
   const inputRef = useRef() as React.MutableRefObject<HTMLButtonElement>;
   const randomArr = useCallback(createRandomRewardsArray(boxCount), [
@@ -135,23 +138,19 @@ const PlayPenGame = () => {
     refresh,
   ]);
 
-
-
   //###좌표값에 반환되는 요소의 dataset에 따라 dispatch되는 함수다. 모듈화 시켜주자
   function dropClick(x: number, y: number): void {
     let cb: any = document.elementFromPoint(x, y);
     let reward = cb.dataset.number;
     let action = cb.dataset.action;
-  
-    if (reward === undefined) {
-      setIsFalseModal((isFalseModal) => !isFalseModal)
-    }
-    else{
-        setIsModal((isModal) => !isModal);
-        dispatch(pengame_request(reward, action,penSpeed.text));
-    };
-  }
 
+    if (reward === undefined) {
+      setIsFalseModal((isFalseModal) => !isFalseModal);
+    } else {
+      setIsModal((isModal) => !isModal);
+      dispatch(pengame_request(reward, action, penSpeed.text));
+    }
+  }
 
   const toggle = () => {
     setPenSatus((penStatus) => !penStatus);
@@ -164,10 +163,9 @@ const PlayPenGame = () => {
       const x = inputRef.current.getBoundingClientRect().x;
       const y = inputRef.current.getBoundingClientRect().y - 10;
       dropClick(x, y);
-
+      setIsLoading((isLoading) => !isLoading);
     }, 500);
   };
-
 
   const refreshRewards = () => {
     setrefresh((refresh) => !refresh);
@@ -175,59 +173,68 @@ const PlayPenGame = () => {
 
   const toggleExit = async () => {
     setPenSatus((penStatus) => !penStatus);
+    setIsLoading((isLoading) => !isLoading);
+
     await getReward();
   };
 
-  const FastForward = ():void => {
-    if(penSpeed.speed === 1){
+  const FastForward = (): void => {
+    if (penSpeed.speed === 1) {
       setPenSpeed({
         speed: 0.5,
-        text:2,
-      })
+        text: 2,
+      });
     }
-    if(penSpeed.speed === 0.5){
+    if (penSpeed.speed === 0.5) {
       setPenSpeed({
         speed: 0.33,
-        text:3,
-    })
-  }
-    if(penSpeed.speed === 0.33){
+        text: 3,
+      });
+    }
+    if (penSpeed.speed === 0.33) {
       setPenSpeed({
         speed: 1,
-        text:1,
-      })
+        text: 1,
+      });
     }
-  }
+  };
 
-  const replay = ():void =>{
+  const replay = (): void => {
     setIsModal((isModal) => !isModal);
     setrefresh((refresh) => !refresh);
     setPenSatus((penStatus) => !penStatus);
-  }
-  const falseReplay = ():void => {
+  };
+  const falseReplay = (): void => {
     setrefresh((refresh) => !refresh);
     setPenSatus((penStatus) => !penStatus);
-    setIsFalseModal((isFalseModal) => !isFalseModal)
-
-  }
-  document.addEventListener('keydown',(e)=>{
-    console.log(e.keyCode)
-  })
-
- 
+    setIsFalseModal((isFalseModal) => !isFalseModal);
+  };
+  document.addEventListener('keydown', (e) => {});
 
   return (
     <>
-    {isModal ? 
-        <ResultModal cName="modalBtn" beforeGold={userInfo?.beforeGold} afterGold={userInfo?.Gold} OnClick={replay}></ResultModal>
-    :
-false    
-    }
-        {isFalseModal ? 
-        <ResultFalseModal cName="modalBtn"  beforeGold={userInfo?.beforeGold} afterGold={userInfo?.Gold} OnClick={falseReplay}></ResultFalseModal>
-    :
-false    
-    }
+      {isLoading ? <Loading></Loading> : null}
+
+      {isModal ? (
+        <ResultModal
+          cName='modalBtn'
+          beforeGold={userInfo?.beforeGold}
+          afterGold={userInfo?.Gold}
+          OnClick={replay}
+        ></ResultModal>
+      ) : (
+        false
+      )}
+      {isFalseModal ? (
+        <ResultFalseModal
+          cName='modalBtn'
+          beforeGold={userInfo?.beforeGold}
+          afterGold={userInfo?.Gold}
+          OnClick={falseReplay}
+        ></ResultFalseModal>
+      ) : (
+        false
+      )}
       {penStatus ? (
         <BasicButtons
           ButtonText='시작'
@@ -248,9 +255,17 @@ false
 
       <BackHistoryBtn corner></BackHistoryBtn>
       <RefreshBtn corner func={refreshRewards}></RefreshBtn>
-      <HomeBtn corner></HomeBtn>    
-      <FastForwardBtn corner func={FastForward} text={penSpeed.text}></FastForwardBtn>
-      <PenEnd penStatus={penStatus} ref={inputRef} penSpeed={penSpeed.speed}></PenEnd>
+      <HomeBtn corner></HomeBtn>
+      <FastForwardBtn
+        corner
+        func={FastForward}
+        text={penSpeed.text}
+      ></FastForwardBtn>
+      <PenEnd
+        penStatus={penStatus}
+        ref={inputRef}
+        penSpeed={penSpeed.speed}
+      ></PenEnd>
       <PenWrap penSpeed={penSpeed.speed} penStatus={penStatus}>
         <PenHead></PenHead>
         <Pen></Pen>
@@ -260,9 +275,14 @@ false
           <div>뒤로 돌아가 다시 박스 갯수를 정해주세요!</div>
         ) : (
           <>
-            {randomArr.map((i:any, index: any) => (
-              <Box red={i.color} key={index} data-action={i.action} data-number={i.number}>
-                {i.front + " " +( i.number * penSpeed.text) + " " + i.back}
+            {randomArr.map((i: any, index: any) => (
+              <Box
+                red={i.color}
+                key={index}
+                data-action={i.action}
+                data-number={i.number}
+              >
+                {i.front + ' ' + i.number * penSpeed.text + ' ' + i.back}
               </Box>
             ))}
           </>
@@ -273,4 +293,4 @@ false
   );
 };
 
-export default React.memo( PlayPenGame);
+export default React.memo(PlayPenGame);
