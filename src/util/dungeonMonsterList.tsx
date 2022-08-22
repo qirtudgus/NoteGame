09e4fd9fa63188956cf92cdc2937432215259042
qirtudgus <qirtudgus@naver.com>
2022-08-23 07:02:11 +0900

    추론된 타입으로 타입 명시

--- src/util/dungeonMonsterList.tsx
+++ src/util/dungeonMonsterList.tsx
@@ -4,7 +4,7 @@ import 고양이 from '../img/monster/고양이.png'
 import 돼지 from '../img/monster/돼지.png'
 import 리자드 from '../img/monster/리자드.png'
 
-export const monsterArr = [
+export const monsterArr: JSX.Element[] = [
   <img src={강아지} alt='monster' title='몬스터'></img>,
   <img src={고양이} alt='monster' title='몬스터'></img>,
   <img src={돼지} alt='monster' title='몬스터'></img>,
