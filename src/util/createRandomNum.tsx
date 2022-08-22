a063a89ee7d81f481979d653af012de5070c3ccb
qirtudgus <qirtudgus@naver.com>
2022-08-23 06:45:20 +0900

    any타입 제거 후 number 타입 지정

--- src/util/createRandomNum.tsx
+++ src/util/createRandomNum.tsx
@@ -1,5 +1,5 @@
 // 인자 숫자 범위 사이의 랜덤한 숫자 생성
-export default function createRandomNum(min: any, max: any):number {
+export default function createRandomNum(min: number, max: number):number {
   var randNum = Math.floor(Math.random() * (max - min + 1)) + min;
   return randNum;
 }
