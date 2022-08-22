05a6bc599b88ccbfce36e75902459a63c659cf9f
qirtudgus <qirtudgus@naver.com>
2022-08-23 06:44:27 +0900

    타입 지정

--- src/util/checkToken.tsx
+++ src/util/checkToken.tsx
@@ -1,8 +1,8 @@
 import jwt from 'jsonwebtoken';
 const SECRET_TOKEN: string = process.env.SECRET_TOKEN!;
-type TokenType = string;
+type TokenType = string| undefined;
 
-const checkToken = (token: string | undefined): string | jwt.JwtPayload => {
+const checkToken = (token: TokenType): string | jwt.JwtPayload => {
   let result = jwt.verify(token!, SECRET_TOKEN);
   return result;
 };
