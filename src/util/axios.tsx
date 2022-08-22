e405824202e81ecc01c5130bbc9e3b06d016bd1f
qirtudgus <qirtudgus@naver.com>
2022-08-23 06:39:45 +0900

    custom axios 필수 매개변수 설정 기존에 매개변수가 전부 옵션이었지만 옵션이 아닌 필수로 사용되는 매개변수는 변경하였다.

--- src/util/axios.tsx
+++ src/util/axios.tsx
@@ -2,16 +2,11 @@ import axios from "axios";
 
 const DOMAIN = "http://localhost:1234";
 
-export const customAxios = async (method? :string, url? :string, data? :any):Promise<any> => {
-    // const token = localStorage.getItem('token') as string
-
+export const customAxios = async (method :string, url :string, data? :any):Promise<any> => {
     return await axios({
         method,
         url : DOMAIN + url,
         data,
-        // headers: {
-        //     authorization:token!          
-        // }
     })
 }
 export default customAxios;
