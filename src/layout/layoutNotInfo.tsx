66049a4b05db56e8352a29e525def1c33206a229
qirtudgus <qirtudgus@naver.com>
2022-08-23 23:57:54 +0900

    안쓰는 코드 제거

--- src/layout/layoutNotInfo.tsx
+++ src/layout/layoutNotInfo.tsx
@@ -1,12 +1,7 @@
 import { Outlet } from 'react-router-dom';
 import BackGround from '../components/BackGround';
-import UserInfo from '../components/userInfo';
-import { useSelector } from 'react-redux';
-import { RootState } from '../modules/modules_index';
 
 const LayoutNotInfo = () => {
-  const isLogin = useSelector((state: RootState) => state.login.isLogin);
-
   return (
     <BackGround>
       <Outlet />
