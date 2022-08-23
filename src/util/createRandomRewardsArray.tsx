5cbfc2bc5597f944a7686be3d231bafdf7dd4150
qirtudgus <qirtudgus@naver.com>
2022-08-23 09:49:19 +0900

    스위치문을 통해 타입 좁혔음 유니온타입에 2개의 배열을 사용하려니 각 배열끼리의 필수속성은 반드시 들어가야한다고 오류가 났다. 처음엔 배열의 타입을 옵션 처리하여 해결했지만 타입 좁히기를 통해 배열의 타입을 명시하면서 사용하는데에 성공했다.

--- src/util/createRandomRewardsArray.tsx
+++ src/util/createRandomRewardsArray.tsx
@@ -1,32 +1,24 @@
 import { rewardsListObj,rewardsList } from './rewardsList';
 import createRandomNumArray from './createRandomNumArray';
-import { dungeonRewardsListObj } from './dungeonRewardList';
+import { dungeonRewardsListObj,dungeonRewardsList } from './dungeonRewardList';
 
 export type useListName = 'penGame' | 'dungeon';
+export type lists = rewardsList[] & dungeonRewardsList[];
 
-
-export const createRandomRewardsArray = (
-  boxCount: number | null,
-  useListName: useListName,
-) => {
-  let rewards:rewardsList[] = [];
- 
-
-  const randomResult = createRandomNumArray(boxCount, useListName);
-
-  if (useListName === 'penGame') {
-    randomResult.map((i: number) => {
-      rewards.push(rewardsListObj[i]);
-    });
-    return rewards;
-  }
-
-  if (useListName === 'dungeon') {
-    randomResult.map((i: number) => {
-      rewards.push(dungeonRewardsListObj[i]);
-    });
-    return rewards;
-
-  }
-  return rewards;
-};
+export function createRandomRewardsArray( boxCount: number | null,
+  useListName: useListName,):lists {
+    let rewards:lists = [];
+    const randomResult = createRandomNumArray(boxCount, useListName);
+  
+    switch(useListName){
+      case 'penGame':
+        randomResult.map((i: number) => {
+          rewards.push(rewardsListObj[i]);
+        }); break
+      case 'dungeon':
+        randomResult.map((i: number) => {
+          rewards.push(dungeonRewardsListObj[i]);
+        });
+      }
+      return rewards;
+    }
