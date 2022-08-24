323cd557cbf80e254109bf35e84155b8673613d8
qirtudgus <qirtudgus@naver.com>
2022-08-24 21:30:52 +0900

    볼펜리워드 배열을 받아 랜덤으로 섞은 배열을 리턴하는 함수 추가

--- src/util/createRandomRewardsArray.tsx
+++ src/util/createRandomRewardsArray.tsx
@@ -1,6 +1,7 @@
 import { rewardsListObj, rewardsList } from './rewardsList';
 import createRandomNumArray from './createRandomNumArray';
 import { dungeonRewardsListObj, dungeonRewardsList } from './dungeonRewardList';
+import createRandomNum from '../util/createRandomNum';
 
 export type useListName = 'penGame' | 'dungeon';
 export type lists = rewardsList[] & dungeonRewardsList[];
@@ -20,5 +21,23 @@ export function createRandomRewardsArray(boxCount: number | null, useListName: u
         return rewards.push(dungeonRewardsListObj[i]);
       });
   }
+  return rewards;
+}
+
+export function createPenRewardArray(arr: number[]): number[] | undefined {
+  let rewards: number[] = [];
+  let rewardsCount = 6;
+  const randomResult: number[] = [];
+  if (arr === undefined) return;
+  while (randomResult.length < rewardsCount) {
+    let num = createRandomNum(0, arr.length - 1);
+    if (randomResult.indexOf(num) === -1) {
+      randomResult.push(num);
+    }
+  }
+  randomResult.map((i: number) => {
+    return rewards.push(arr[i]);
+  });
+
   return rewards;
 }
