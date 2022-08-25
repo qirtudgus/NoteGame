348c3327049b6b2adeb7ccb5030678b203e55b4f
qirtudgus <qirtudgus@naver.com>
2022-08-26 04:27:18 +0900

    WeaponDamage를 추가한 공식으로 변경

--- src/util/createDamage.tsx
+++ src/util/createDamage.tsx
@@ -4,16 +4,19 @@ import createRandomNum from './createRandomNum';
 // 1. 리워드데미지 %
 // 2. 유저 기본데미지
 // 3. 공격력 스킬렙
-
+// 4. 유저 무기데미지
 // 스킬로 인한 추가 데미지
 
 // 리워드로 계산한 최종데미지
 
-export const userDamage = (reward: number, userDamage: number, betterPen: number): number => {
+export const userDamage = (reward: number, basicDamage: number, weaponDamage: number, betterPen: number): number => {
   // 유저의 기본데미지에 스킬렙만큼(n*2%)의 데미지를 구한다.
+  let userDamage = basicDamage + weaponDamage;
+
   let skillDamage = (userDamage * (betterPen * 2)) / 100;
   // 기본데미지에 스킬데미지를 합산
   let addDamage = userDamage + skillDamage;
+
   // reward로 뽑은만큼의 데미지를 계산하여 반환
   let resultUserDamage = Math.ceil(addDamage * (reward / 100));
 
