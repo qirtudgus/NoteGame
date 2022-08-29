1cf1702be0760c82d15e2d94676ea4e1100c0572
qirtudgus <qirtudgus@naver.com>
2022-08-30 05:23:39 +0900

    위치 및 디자인 대대적 변경 기존 하단에서 상단으로 옮기고 정보를 가로로 나열한다.

--- src/components/userInfo.tsx
+++ src/components/userInfo.tsx
@@ -1,47 +1,66 @@
-import styled from 'styled-components';
-import { useSelector } from 'react-redux';
+import styled, { css } from 'styled-components';
+import { useSelector, useDispatch } from 'react-redux';
 import { RootState } from '../modules/modules_index';
 // import { expTable } from '../util/ExpTable';
 import { expTable } from '../util/expTable';
 import React from 'react';
 import 위쪽화살표 from '../img/위쪽화살표.svg';
+import 칼 from '../img/흰칼.svg';
+import 잉크 from '../img/잉크.png';
 import { LoginUserInfoInterface } from '../modules/login';
-
+import { userInfo_visible_on, userInfo_visible_off } from '../modules/userInfoVisible';
+import { ButtonColor } from './BtnMenu';
 const BasicBox = styled.div`
-  width: auto;
+  width: 100%;
   position: absolute;
-  bottom: -1px;
-  left: 5px;
+  top: 77px;
+  left: 1px;
   z-index: 10;
-  font-size: 22px;
-  font-weight: bold;
+  font-size: 1.2rem;
+  padding-bottom: 7px;
   line-height: 32px;
+  letter-spacing: 1px;
 `;
+
 const UserBox = styled.div`
   display: flex;
   align-items: center;
-  background: #ffbc26;
+  /* background: #ffbc26; */
   padding: 3px 10px 3px 20px;
-  border-radius: 0 20px 20px 22px;
-  &:hover {
-    background: #b6861f;
-  }
 `;
 
-const UserId = styled.p`
-  font-size: 2rem;
+const UserId = styled.div`
+  border: 1px solid#aaa;
+  background: #333;
+  font-size: 1.5rem;
+  color: #fff;
+  box-shadow: 2px 2px 5px 2px rgb(0 0 0 / 35%);
+  margin-left: 0.6rem;
+  padding: 4px 1rem 4px 1rem;
+  border-radius: 6px;
 `;
-const UserLevel = styled.span`
-  font-size: 1.2rem;
-  padding: 5px;
-  border-radius: 5px;
+const UserLevel = styled.div`
+  border: 1px solid#aaa;
+  background: #333;
+  color: #fff;
+  box-shadow: 2px 2px 5px 2px rgb(0 0 0 / 35%);
+  margin-left: 0.1rem;
+  padding: 4px 1rem 4px 1rem;
+  border-radius: 6px;
 `;
 
-const UserStat = styled.div`
+const UserStat = styled.div<btnAni>`
+  position: relative;
   padding: 3px;
+  ${(props) => props.isVisible && css``}
 `;
 
-const UserBtnCircle = styled.div`
+interface btnAni {
+  isVisible: boolean;
+}
+
+const UserBtnCircle = styled.div<btnAni>`
+  cursor: pointer;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -51,38 +70,104 @@ const UserBtnCircle = styled.div`
   height: 35px;
   width: 35px;
   background: #fff;
-  & > img {
-    width: 40px;
+  ${(props) =>
+    props.isVisible &&
+    css`
+      & > img {
+        transition: 0.2s all;
+        transform: rotate(0deg);
+      }
+    `}
+`;
+
+const UserGold = styled.div`
+  cursor: default;
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  min-width: 100px;
+  background: #333;
+  color: #fff;
+  margin-left: 0.1rem;
+  padding: 3px 1rem 3px 1rem;
+  border-radius: 6px;
+  border: 1px solid#aaa;
+  box-shadow: 2px 2px 5px 2px rgb(0 0 0 / 35%);
+
+  & img {
+    width: 28px;
+  }
+  & > span {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-right: 1rem;
   }
 `;
 
 const UserInfo = () => {
+  const dispatch = useDispatch();
   const userId = useSelector((state: RootState) => state.login.id);
+  const isVisible = useSelector((state: RootState) => state.userInfo_visibleRequest.isVisible);
   const { Level, BasicHp, BasicDamage, WeaponDamage, Gold, Exp } = useSelector(
     (state: RootState) => state.login.userInfo,
   ) as LoginUserInfoInterface;
 
   return (
     <BasicBox>
-      <UserStat>
-        <p>체력 {BasicHp}</p>
-        <p>공격력 {BasicDamage + WeaponDamage}</p>
-        <p>골드 {Gold.toLocaleString()}</p>
-        <p>
-          경험치 {Exp} / {expTable[Level]}
-        </p>
-      </UserStat>
       <UserBox>
-        <UserId>
-          {userId} <UserLevel>Lv.{Level}</UserLevel>
-        </UserId>
-        <UserBtnCircle>
+        <UserId>{userId}</UserId>
+        <UserLevel>Lv. {Level}</UserLevel>
+        <UserGold as='div'>
+          <span>HP</span> {BasicHp.toLocaleString()}
+        </UserGold>
+        <UserGold as='div'>
+          <span>
+            <img
+              src={칼}
+              alt='공격력'
+            ></img>
+          </span>
+          {(BasicDamage + WeaponDamage).toLocaleString()}
+        </UserGold>
+        <UserGold as='div'>
+          <span>
+            <img
+              src={잉크}
+              alt='잉크'
+            ></img>
+          </span>
+          {Gold.toLocaleString()}
+        </UserGold>
+        {/* <UserBtnCircle
+          isVisible={isVisible}
+          onClick={
+            isVisible
+              ? () => {
+                  dispatch(userInfo_visible_off());
+                }
+              : () => {
+                  dispatch(userInfo_visible_on());
+                }
+          }
+        >
           <img
             src={위쪽화살표}
             alt='보기'
           ></img>
-        </UserBtnCircle>
+        </UserBtnCircle> */}
       </UserBox>
+      {isVisible ? (
+        <UserStat isVisible={isVisible}>
+          <p>체력 {BasicHp}</p>
+          <p>공격력 {BasicDamage + WeaponDamage}</p>
+
+          <p>
+            경험치 {Exp} / {expTable[Level]}
+          </p>
+        </UserStat>
+      ) : null}
     </BasicBox>
   );
 };
