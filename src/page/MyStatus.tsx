b99620a4de769ecc6a5e8b6ec17188f549a26cb1
qirtudgus <qirtudgus@naver.com>
2022-08-30 07:07:48 +0900

    내 상태를 보여주는 MyStatus 컴포넌트 생성 및 구현 기존의 Skill 컴포넌트에서 좀 더 확장된 기능의 컴포넌트다. 구조는 탭메뉴와 콘텐츠로 이전과 유사하다. 하지만 탭메뉴를 구현하는 방식과 콘텐츠를 렌더링하는 방식이 변경되었다.
    특히 콘텐츠는 키값에 컴포넌트를 할당하여 출력하는
    써본적 없는 방식이다.
    한 파일에서 쓰면 굉장히 복잡한데
    코드를 나눠 각 파일에서 관리하기때문에
    유지보수에 용이해졌다.

---
+++ src/page/MyStatus.tsx
@@ -0,0 +1,97 @@
+import React, { useState } from 'react';
+import styled, { css } from 'styled-components';
+import BtnMenu from '../components/BtnMenu';
+import CharacterBox from '../components/CharacterBox';
+import NewSkillPiece from '../components/NewSkillPiece';
+import StatList from '../components/StatList';
+const StatusWrap = styled.div`
+  width: 90%;
+  margin-top: 50px;
+  height: 570px;
+  display: flex;
+`;
+
+const Character = styled.div`
+  width: 35%;
+  height: 100%;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Status = styled.div`
+  width: 65%;
+  height: 100%;
+  /* background: #aaa; */
+`;
+
+const TabWrap = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: flex-start;
+`;
+const Tab = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  font-size: 1.6rem;
+  align-items: center;
+  background: #eee;
+  height: 60px;
+  &.active {
+    background: #fcbb26;
+  }
+`;
+
+const TabContent = styled.div`
+  height: 100%;
+  /* padding: 20px 0 20px 0; */
+`;
+
+const MyStatus = () => {
+  const [tabNum, SetTabNum] = useState(0);
+
+  const menuArr = [
+    {
+      name: '기본정보',
+      content: <StatList></StatList>,
+    },
+    {
+      name: '능력치',
+      content: <NewSkillPiece></NewSkillPiece>,
+    },
+    { name: '스킬', content: '스킬입니다.' },
+  ];
+
+  const SetTabNumberFunc = (index: number): void => {
+    SetTabNum(index);
+  };
+
+  return (
+    <StatusWrap>
+      <Character>
+        <CharacterBox></CharacterBox>
+      </Character>
+      <Status>
+        <TabWrap>
+          {menuArr.map((i: any, index: number) => {
+            return (
+              <Tab
+                key={index}
+                className={tabNum === index ? 'active' : ''}
+                onClick={() => SetTabNumberFunc(index)}
+              >
+                {i.name}
+              </Tab>
+            );
+          })}
+        </TabWrap>
+        <TabContent>{menuArr[tabNum].content}</TabContent>
+      </Status>
+      <BtnMenu BackHistory></BtnMenu>
+    </StatusWrap>
+  );
+};
+
+export default React.memo(MyStatus);
