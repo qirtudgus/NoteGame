60a59f6b7c38fcf4bf113d4e2a84dc9a0e516631
qirtudgus <qirtudgus@naver.com>
2022-08-31 03:55:34 +0900

    BallPenShop 에서 Shop 컴포넌트로 변경 변경된 점 1.탭메뉴 구현 방식 2.탭 콘텐츠 컴포넌트화하여 의존성을 덜어냄 (기존에는 ref를 콘텐츠에 전달하여 무한스크롤을 구현했으나 콘텐츠를 따로 컴포넌트화하여 내부에서 직접 사용하여 fowardref가 필요없어짐 )

---
+++ src/page/Shop.tsx
@@ -0,0 +1,97 @@
+import React, { useState } from 'react';
+import styled, { css } from 'styled-components';
+import BtnMenu from '../components/BtnMenu';
+import CharacterBox from '../components/CharacterBox';
+import NewShopPiece from '../components/NewShopPiece';
+import NewSkillPiece from '../components/NewSkillPiece';
+const ShopWrap = styled.div`
+  width: 90%;
+  margin-top: 50px;
+  height: 570px;
+  display: flex;
+`;
+
+const Character = styled.div`
+  width: 30%;
+  height: 100%;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Shops = styled.div`
+  width: 70%;
+  height: 100%;
+  /* background: #aaa; */
+`;
+
+const TabWrap = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: flex-start;
+`;
+const Tab = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  font-size: 1.6rem;
+  align-items: center;
+  background: #eee;
+  height: 60px;
+  &.active {
+    background: #fcbb26;
+  }
+`;
+
+const TabContent = styled.div`
+  height: 100%;
+  /* padding: 20px 0 20px 0; */
+`;
+
+const Shop = () => {
+  const [tabNum, SetTabNum] = useState(0);
+
+  const menuArr = [
+    {
+      name: '볼펜',
+      content: <NewShopPiece></NewShopPiece>,
+    },
+    {
+      name: '종이',
+      content: <NewSkillPiece></NewSkillPiece>,
+    },
+    { name: '스킬', content: '스킬입니다.' },
+  ];
+
+  const SetTabNumberFunc = (index: number): void => {
+    SetTabNum(index);
+  };
+
+  return (
+    <ShopWrap>
+      <Character>
+        <CharacterBox></CharacterBox>
+      </Character>
+      <Shops>
+        <TabWrap>
+          {menuArr.map((i: any, index: number) => {
+            return (
+              <Tab
+                key={index}
+                className={tabNum === index ? 'active' : ''}
+                onClick={() => SetTabNumberFunc(index)}
+              >
+                {i.name}
+              </Tab>
+            );
+          })}
+        </TabWrap>
+        <TabContent>{menuArr[tabNum].content}</TabContent>
+      </Shops>
+      <BtnMenu BackHistory></BtnMenu>
+    </ShopWrap>
+  );
+};
+
+export default React.memo(Shop);
