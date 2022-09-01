fd0839398d1ee4aa56846fbed7dd89e7a90607a9
qirtudgus <qirtudgus@naver.com>
2022-09-01 22:06:41 +0900

    새로운 Ballpen컴포넌트 추가 기존과 다른점 1. 기존에는 볼펜 width에 따라 움직이는 범위가 조금씩 달랐으나 통일시킴 2. 전보다 더 많은 부분의 볼펜이 나타남

---
+++ src/components/NewBallpen.tsx
@@ -0,0 +1,64 @@
+import { useSelector } from 'react-redux';
+import styled, { css, keyframes } from 'styled-components';
+import { RootState } from '../modules/modules_index';
+// import { PenImgDun, PenImgWrapDun, PenEndDun } from '../styledComponents/DungeonFight_Effect';
+import { LoginUserInfoInterface } from '../modules/login';
+import { penObj } from '../util/shopList';
+import React from 'react';
+
+interface ballpen {
+  penTop: number;
+  penLeft: number;
+}
+
+const PenImgWrap = styled.div<ballpen>`
+  top: ${(props) => props.penTop + 'px'};
+  left: ${(props) => props.penLeft + 'px'};
+  position: absolute;
+  height: auto;
+  width: 50px;
+  display: flex;
+  justify-content: center;
+`;
+
+const PenPoint = styled.div`
+  width: 1px;
+  height: 30px;
+  background: rgba(0, 0, 0, 1);
+  position: absolute;
+  z-index: 100;
+  top: -30px;
+`;
+
+const PenImg = styled.img`
+  z-index: 100;
+  display: block;
+  position: relative;
+  height: auto;
+  width: 60%;
+  top: 0px;
+  left: 0px;
+  margin: none;
+`;
+
+const NewBallpen = ({ penTop, penLeft }: ballpen) => {
+  const { EquipBallpen } = useSelector((state: RootState) => state.login.userInfo) as LoginUserInfoInterface;
+  let equipBallpenImg = penObj.find((i: any) => i.ballPenName === EquipBallpen)?.img;
+
+  return (
+    <>
+      <PenImgWrap
+        penTop={penTop}
+        penLeft={penLeft}
+      >
+        <PenImg
+          id='penBody'
+          src={equipBallpenImg}
+        ></PenImg>
+        <PenPoint id='penPoint'></PenPoint>
+      </PenImgWrap>
+    </>
+  );
+};
+
+export default React.memo(NewBallpen);
