94abfb19ff7d8dcbdb25752d070ce2195f8c5b91
qirtudgus <qirtudgus@naver.com>
2022-09-06 06:34:46 +0900

    기본적으로 닫기버튼을 가지고있는다.

--- src/components/RevivalModal.tsx
+++ src/components/RevivalModal.tsx
@@ -1,4 +1,8 @@
 import styled from 'styled-components';
+import { useDispatch } from 'react-redux';
+import { modal_failure } from '../modules/modalState';
+import 엑스 from '../img/엑스.svg';
+
 const BgWrap = styled.div`
   width: 100%;
   height: 100%;
@@ -7,14 +11,14 @@ const BgWrap = styled.div`
   justify-content: center;
   align-items: center;
   z-index: 1000;
-  background: rgba(0, 0, 0, 0.5);
+  background: rgba(0, 0, 0, 0.2);
   left: 0;
   top: 0;
 `;
 
 const Bg = styled.div`
   border-radius: 20px;
-  width: 600px;
+  width: 500px;
   height: 300px;
   background: ${(props) => props.color || '#eee'};
   margin: 50px auto;
@@ -27,10 +31,32 @@ const Bg = styled.div`
   user-select: none;
 `;
 
+const Close = styled.div`
+  position: absolute;
+  top: 20px;
+  right: 20px;
+  cursor: pointer;
+  & img {
+    width: 35px;
+  }
+`;
+
 const RevivalModal = (props: any) => {
+  const dispatch = useDispatch();
+  function close() {
+    dispatch(modal_failure());
+  }
   return (
     <BgWrap>
-      <Bg>{props.children}</Bg>
+      <Bg>
+        <Close onClick={close}>
+          <img
+            src={엑스}
+            alt='닫기'
+          />
+        </Close>
+        {props.children}
+      </Bg>
     </BgWrap>
   );
 };
