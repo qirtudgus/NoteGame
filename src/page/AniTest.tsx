025c2243404d6cf1c935475eff1aa85499698b27
qirtudgus <qirtudgus@naver.com>
2022-09-05 11:11:33 +0900

    리워드획득 애니 테스트용 컴포넌트

--- src/page/AniTest.tsx
+++ src/page/AniTest.tsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import styled, { css, keyframes } from 'styled-components';
+import anime from 'animejs';
+
 interface test {
   ani: boolean;
 }
@@ -18,46 +20,65 @@ const animations = keyframes`
     `;
 
 const Point = styled.div<test>`
-  width: 5px;
-  height: 5px;
+  width: 2px;
+  height: 20px;
   background: #000;
 
-  animation: ${animations} 1s ease-in-out infinite; //1초동안 선형 무한 속성값주기
+  /* animation: ${animations} 0.5s ease-in-out infinite; //1초동안 선형 무한 속성값주기
   animation-play-state: running;
   animation-fill-mode: forwards;
   ${(props) =>
     props.ani &&
     css`
       animation-play-state: paused;
-    `}
+    `} */
 `;
 
-const Btn = styled.div`
+const Point2 = styled.div<test>`
+  width: 2px;
+  height: 20px;
+  background: #000;
+`;
+
+const Btn = styled.button`
+  cursor: pointer;
   width: 100px;
   height: 100px;
-  background: #eee;
+  margin-top: 100px;
 `;
 
 const AniTest = () => {
   const [aniTest, SetAniTest] = useState(false);
 
-  //애니메이션에 상관없이 요소이 기본위치를 불러오는 함수
-  function getOffset(element: any) {
-    var offsetLeft = 0;
-    var offsetTop = 0;
-
-    while (element) {
-      offsetLeft += element.offsetLeft;
-      offsetTop += element.offsetTop;
-
-      element = element.offsetParent;
-    }
-
-    return [offsetLeft, offsetTop];
-  }
-
+  // animation.play();
   useEffect(() => {
     let testPen = document.querySelector('#testPen') as HTMLElement;
+    let testPen2 = document.querySelector('#testPen2') as HTMLElement;
+    let btn = document.querySelector('#btn2') as HTMLElement;
+    let btn2 = document.querySelector('#btn3') as HTMLElement;
+
+    let animation = anime({
+      targets: '#testPen2',
+      translateX: 250,
+      duration: 250,
+      direction: 'alternate', //번갈아 재생
+      loop: true, // number는 횟수 true는 무한
+      easing: 'easeInOutSine',
+      autoplay: false,
+    });
+
+    btn.addEventListener('click', () => {
+      console.log(aniTest);
+      animation.play();
+      // animation.pause();
+    });
+
+    btn2.addEventListener('click', () => {
+      let testPen2 = document.querySelector('#testPen2') as HTMLElement;
+      animation.pause();
+      let x = testPen2.getBoundingClientRect().x;
+      console.log(x);
+    });
 
     document.addEventListener('click', function (event) {
       var x = event.clientX;
@@ -66,41 +87,41 @@ const AniTest = () => {
       console.log(coords);
     });
 
-    testPen.addEventListener('animationend', function (event) {
-      const svgRect = this.getBoundingClientRect();
-      console.log(svgRect);
-    });
+    // testPen.addEventListener('animationend', function (event) {
+    //   const svgRect = this.getBoundingClientRect();
+    //   console.log(svgRect);
+    // });
 
-    testPen.addEventListener('animationcancel', function (event) {
-      const svgRect = this.getBoundingClientRect();
-      console.log(svgRect);
-    });
+    // testPen.addEventListener('animationcancel', function (event) {
+    //   const svgRect = this.getBoundingClientRect();
+    //   console.log(svgRect);
+    // });
 
-    testPen.addEventListener('animationstart', function (event) {
-      const svgRect = this.getBoundingClientRect();
-      //   console.log(svgRect); // 정상
-    });
-    testPen.addEventListener('animationiteration', function (event) {
-      const svgRect = this.getBoundingClientRect();
-      //   console.log(svgRect); // 정상
-    });
+    // testPen.addEventListener('animationstart', function (event) {
+    //   const svgRect = this.getBoundingClientRect();
+    //   //   console.log(svgRect); // 정상
+    // });
+    // testPen.addEventListener('animationiteration', function (event) {
+    //   const svgRect = this.getBoundingClientRect().x;
+    //   //   console.log(svgRect); // 정상
+    // });
   }, []);
+
+  function coordsFunc2(e: any) {
+    let testPen = document.querySelector('#testPen2') as HTMLElement;
+    const svgRect = testPen.getBoundingClientRect().x;
+    console.log(svgRect); // 정상
+  }
 
   return (
     <>
-      <Point
-        id='testPen'
+      <Point2
+        id='testPen2'
         ani={aniTest}
-      ></Point>
-      <Btn
-        onClick={() => {
-          let testPen = document.querySelector('#testPen') as HTMLElement;
-          console.log(testPen.getBoundingClientRect());
-          SetAniTest((ani) => !ani);
-        }}
-      >
-        멈춰라!!
-      </Btn>
+      ></Point2>
+
+      <Btn id='btn2'>anime play</Btn>
+      <Btn id='btn3'>stop</Btn>
     </>
   );
 };
