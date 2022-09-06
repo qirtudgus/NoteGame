c9369b78d329b7a048b8c16cfc752c26fb7c7f69
qirtudgus <qirtudgus@naver.com>
2022-09-07 06:22:12 +0900

    종이 관련 api추가 /upatepaper  = DB의 BuyPaperList를 가져와 반환해준다. /buypaper = 사용자가 구매한 paperName을 BuyPaperList에 추가한다. /equippaper = 사용자가 장착한 paperName을 EquipPaper에 할당한다.

--- server/router/buyBallepenListRouter.tsx
+++ server/router/buyBallepenListRouter.tsx
@@ -4,19 +4,29 @@ import { userInfoProcess } from '../../src/util/userInfoProcess.js';
 
 export const buyBallpenListRouter = express.Router();
 
-const userFindQuery = 'SELECT BuyBallpenList FROM users WHERE ID = ?';
-const EquipBallpenQuery = `UPDATE users SET EquipBallpen = ?, WeaponDamage = ?, DungeonPenSpeed = ?, PenGamePenSpeed = ? WHERE ID = ?`;
-
 const loginQuery = 'SELECT * FROM users WHERE ID = ?';
+const userFindQuery = 'SELECT BuyBallpenList FROM users WHERE ID = ?';
+const userPaperFindQuery = 'SELECT BuyPaperList FROM users WHERE ID = ?';
+const EquipBallpenQuery = `UPDATE users SET EquipBallpen = ?, WeaponDamage = ?, DungeonPenSpeed = ?, PenGamePenSpeed = ? WHERE ID = ?`;
+const EquipPaperQuery = `UPDATE users SET EquipPaper = ?, WeaponHp = ? WHERE ID = ?`;
+
 const ConcatBuyBallpenList = `UPDATE users SET BuyBallpenList = CONCAT(BuyBallpenList,',',?) WHERE ID = ?`;
+const ConcatBuyPaperList = `UPDATE users SET BuyPaperList = CONCAT(BuyPaperList,',',?) WHERE ID = ?`;
 const BuyAfterGoldQuery = `UPDATE users SET Gold = Gold - ? WHERE ID = ?`;
 
 buyBallpenListRouter.post('/updateballpen', (req, res, next) => {
   const userId = req.decoded.userId;
   db.query(userFindQuery, [userId], (err, rows, fields) => {
-    console.log(rows[0].BuyBallpenList.split(','));
     const resultList: [string] = rows[0].BuyBallpenList.split(',');
     res.status(200).json({ code: 200, buyBallpenList: resultList });
+  });
+});
+
+buyBallpenListRouter.post('/updatepaper', (req, res, next) => {
+  const userId = req.decoded.userId;
+  db.query(userPaperFindQuery, [userId], (err, rows, fields) => {
+    const resultList: [string] = rows[0].BuyPaperList.split(',');
+    res.status(200).json({ code: 200, buyPaperList: resultList });
   });
 });
 
@@ -28,7 +38,7 @@ buyBallpenListRouter.post('/realbuyballpen', (req, res, next) => {
       db.query(userFindQuery, [userId], (err, rows, fields) => {
         console.log(rows[0].BuyBallpenList.split(','));
         const resultList: [string] = rows[0].BuyBallpenList.split(',');
-
+        console.log(`${userId}님이 ${ballpenName}을 구매했습니다.`);
         db.query(loginQuery, [userId], (err, rows, fields) => {
           let userInfo = userInfoProcess(rows[0]);
           console.log(userInfo);
@@ -46,8 +56,6 @@ buyBallpenListRouter.post('/realbuyballpen', (req, res, next) => {
 buyBallpenListRouter.post('/equip', (req, res, next) => {
   const userId = req.decoded.userId;
   const { ballpenName, weaponDamage, PenSpeed } = req.body;
-  console.log(weaponDamage);
-  console.log(PenSpeed);
   db.query(
     EquipBallpenQuery,
     [ballpenName, weaponDamage, PenSpeed.DungeonPenSpeed, PenSpeed.PenGamePenSpeed, userId],
@@ -58,4 +66,36 @@ buyBallpenListRouter.post('/equip', (req, res, next) => {
       });
     },
   );
+});
+
+buyBallpenListRouter.post('/equippaper', (req, res, next) => {
+  const userId = req.decoded.userId;
+  const { paperName, Hp } = req.body;
+  db.query(EquipPaperQuery, [paperName, Hp, userId], (err, rows, fields) => {
+    db.query(loginQuery, [userId], (err, rows, fields) => {
+      let userInfo = userInfoProcess(rows[0]);
+      res.status(200).json({ code: 200, userInfo: userInfo });
+    });
+  });
+});
+
+buyBallpenListRouter.post('/buypaper', (req, res) => {
+  const userId = req.decoded.userId;
+  const { paperName, Gold } = req.body;
+  db.query(BuyAfterGoldQuery, [Gold, userId], (err, rows, fields) => {
+    db.query(ConcatBuyPaperList, [paperName, userId], (err, rows, fields) => {
+      db.query(userPaperFindQuery, [userId], (err, rows, fields) => {
+        const resultList: [string] = rows[0].BuyPaperList.split(',');
+        console.log(`${userId}님이 ${paperName}을 구매했습니다.`);
+        db.query(loginQuery, [userId], (err, rows, fields) => {
+          let userInfo = userInfoProcess(rows[0]);
+          res.status(200).json({
+            code: 200,
+            userInfo: userInfo,
+            BuyPaperList: resultList,
+          });
+        });
+      });
+    });
+  });
 });
