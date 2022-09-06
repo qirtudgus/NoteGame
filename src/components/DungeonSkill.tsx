17bf366952e487728d6a73a0ff56acf417bd81ca
qirtudgus <qirtudgus@naver.com>
2022-09-06 09:38:20 +0900

    9칸으로 이루어진 스킬사용 ui컴포넌트 생성 확장성있게 만드는것이 중요할것같다 우선 1차원적이게 props로 더블어택 습득 유무와, 사용 유무를 받아와 사용하는거까진 문제없다.

---
+++ src/components/DungeonSkill.tsx
@@ -0,0 +1,68 @@
+import styled, { css, keyframes } from 'styled-components';
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
+import { RootState } from '../modules/modules_index';
+import { LoginUserInfoInterface } from '../modules/login';
+const Wrap = styled.div`
+  position: absolute;
+  right: 30px;
+  z-index: 100;
+  bottom: 30px;
+  width: 175px;
+  height: 175px;
+  padding: 10px;
+  background: #fff;
+  display: flex;
+  border-radius: 10px;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+`;
+
+interface useSkill {
+  useDoubleAttack?: boolean;
+}
+
+const Icon = styled.div<useSkill>`
+  width: 50px;
+  height: 50px;
+  background: #eee;
+  margin: 4px;
+  ${(props) =>
+    props.useDoubleAttack &&
+    css`
+      background: #e5005a;
+    `}
+`;
+
+const DungeonSKill = (props: any) => {
+  const { UpDoubleAttack } = useSelector((state: RootState) => state.login.userInfo) as LoginUserInfoInterface;
+
+  return (
+    <>
+      <Wrap>
+        {UpDoubleAttack > 0 ? (
+          <Icon
+            useDoubleAttack={props.useDoubleAttack}
+            onClick={props.OnClick}
+          >
+            더블어택
+          </Icon>
+        ) : (
+          <Icon></Icon>
+        )}
+
+        <Icon></Icon>
+        <Icon></Icon>
+        <Icon></Icon>
+        <Icon></Icon>
+        <Icon></Icon>
+        <Icon></Icon>
+        <Icon></Icon>
+        <Icon></Icon>
+      </Wrap>
+    </>
+  );
+};
+
+export default React.memo(DungeonSKill);
