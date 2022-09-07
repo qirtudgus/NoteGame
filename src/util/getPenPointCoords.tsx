a607ef186ad319057f0b2bb9b99c5479ff9e789a
qirtudgus <qirtudgus@naver.com>
2022-09-07 23:23:18 +0900

    penPoint가 아닌 penBody로 좌표 계산하도록 로직 변경 0.5px 더 정확함

--- src/util/getPenPointCoords.tsx
+++ src/util/getPenPointCoords.tsx
@@ -1,9 +1,13 @@
 // #penPoint 요소를 찾아서 해당 요소의 좌표를 반환합니다.
 // NewDungeonFight에 쓰이고있습니다.
 export const getRewardCoords = () => {
-  const pen = document.querySelector('#penPoint') as Element;
-  const x: number = pen.getBoundingClientRect().x as number;
-  const y: number = (pen.getBoundingClientRect().y as number) - 20;
+  // const pen = document.querySelector('#penPoint') as Element;
+  const penbody = document.querySelector('#penBody') as Element;
+
+  // const x: number = pen.getBoundingClientRect().x as number;
+  // const y: number = (pen.getBoundingClientRect().y as number) - 20;
+  let x = penbody.getBoundingClientRect().x + penbody.clientWidth / 2;
+  let y = penbody.getBoundingClientRect().y - 50;
   let resultCoords = { x, y };
   return resultCoords;
 };
