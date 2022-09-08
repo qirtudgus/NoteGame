4820aca619331b6a69dc8113cf961f3009b9d80d
qirtudgus <qirtudgus@naver.com>
2022-09-09 04:53:49 +0900

    서버에도 아이디 체크 정규표현식 추가 프론트에서 이미 다 체크하지만 이중으로 작성해놨다. 실제로 배포했을때 서버 로그에서 문제가 없다면 백은 지워도될것같다.

--- server/router/registerRouter.tsx
+++ server/router/registerRouter.tsx
@@ -20,19 +20,41 @@ registerRouter.post('/join', (req: Request, res: Response, next: NextFunction) =
 registerRouter.post('/confirmid', (req: Request, res: Response, next: NextFunction) => {
   const { id } = req.body;
   const idCheck = 'SELECT * FROM users WHERE ID = ?';
+
+  //아이디 양식
+  const idCheckRegex = /^[가-힣a-zA-Z0-9]{2,10}$/g;
+
+  //특수문자 체크
+  const special_pattern = /[`~!@#$%^&*|\\\'\";:\/?]/gi;
+
+  //공백체크 표현식
+  const spaceCheck = /\s/g;
   if (id === '') {
-    res.json({ auth: false, text: '아이디를 입력해주세요!' });
+    res.json({ auth: false, text: '2~10자 영문,한글,숫자만 사용 가능합니다.' });
     return;
   }
-
-  db.query(idCheck, [id], function (err, rows, fields) {
-    if (rows[0] === undefined) {
-      console.log('사용가능한 아이디');
-      res.status(200).json({ auth: true, text: '사용 가능한 아이디입니다.' });
-      // res.send(true);
-    } else if (rows[0]) {
-      res.status(200).json({ auth: false, text: '사용할 수 없는 아이디입니다.' });
-      // res.send(false);
-    }
-  });
+  if (spaceCheck.test(id)) {
+    res.json({ auth: false, text: '2~10자 영문,한글,숫자만 사용 가능합니다.' });
+    return;
+  }
+  if (special_pattern.test(id)) {
+    res.json({ auth: false, text: '2~10자 영문,한글,숫자만 사용 가능합니다.' });
+    return;
+  }
+  if (!idCheckRegex.test(id)) {
+    res.json({ auth: false, text: '2~10자 영문,한글,숫자만 사용 가능합니다.' });
+    return;
+  } else {
+    //정규표현식 패스하면
+    db.query(idCheck, [id], function (err, rows, fields) {
+      if (rows[0] === undefined) {
+        console.log('사용가능한 아이디');
+        res.status(200).json({ auth: true, text: '사용 가능한 아이디입니다.' });
+        // res.send(true);
+      } else if (rows[0]) {
+        res.status(200).json({ auth: false, text: '이미 사용중인 아이디입니다.' });
+        // res.send(false);
+      }
+    });
+  }
 });
