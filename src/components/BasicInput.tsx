72706f13028b832147ffceb086df75cbabb84905
qirtudgus <qirtudgus@naver.com>
2022-09-09 15:30:07 +0900

    interface 추가 및 기본 css속성 지정   ref?: any; // 포커싱을 위한 ref   maxLength?: number; // 글자수 제한을 위함   margin?: string; // 마진 설정   width?: string; // 너비 설정   disabled?: boolean; // 비활성/활성화 설정

--- src/components/BasicInput.tsx
+++ src/components/BasicInput.tsx
@@ -1,36 +1,51 @@
 import styled from 'styled-components';
-import React from 'react';
-const BasicInput = styled.input`
-  width: 13rem;
+import React, { ForwardedRef, forwardRef } from 'react';
+const BasicInput = styled.input<holder>`
   height: 3rem;
   font-size: 1.5rem;
+  width: ${(props) => props.width || '13rem'};
   background: ${(props) => props.color || '#fff'};
+
   position: relative;
   z-index: 2;
   margin-bottom: 3rem;
+  margin: ${(props) => props.margin || '0 0 3rem 0'};
   border: none;
 `;
 
 interface holder {
-  placeholder: string;
+  placeholder?: string;
   OnChange?: (e: any) => void;
   OnBlur?: (e: any) => void;
   value?: string;
   type?: string;
   color?: string;
+  ref?: any;
+  maxLength?: number;
+  margin?: string;
+  width?: string;
+  disabled?: boolean;
 }
 
-const BasicInputs = ({ placeholder, OnChange, value, type, OnBlur, color }: holder) => {
+const BasicInputs = (
+  { placeholder, OnChange, value, type, OnBlur, color, maxLength, margin, width, disabled }: holder,
+  ref: ForwardedRef<any>,
+) => {
   return (
     <BasicInput
+      width={width}
       color={color}
       type={type}
+      margin={margin}
       placeholder={placeholder}
       onChange={OnChange}
       onBlur={OnBlur}
       value={value}
+      ref={ref}
+      maxLength={maxLength}
+      disabled={disabled}
     ></BasicInput>
   );
 };
 
-export default React.memo(BasicInputs);
+export default React.memo(forwardRef(BasicInputs));
