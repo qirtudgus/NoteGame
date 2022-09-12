7c57820a80890b4b7cc7fb0109e4531c82e110a1
qirtudgus <qirtudgus@naver.com>
2022-09-13 03:59:54 +0900

    아이콘 추가

--- src/components/FooterMenu.tsx
+++ src/components/FooterMenu.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import styled, { css } from 'styled-components';
+import 랭킹 from '../img/랭킹.svg';
+import 던전 from '../img/던전.svg';
+import 볼펜굴리기 from '../img/볼펜굴리기.svg';
+import 상점 from '../img/상점.svg';
+import 내상태 from '../img/내상태.svg';
 
 const FooterWrap = styled.div`
   position: absolute;
@@ -15,7 +20,6 @@ const FooterWrap = styled.div`
 const FooterIcon = styled.div`
   width: 100px;
   height: 100px;
-  background: #fff;
   margin: 0 25px 10px 0;
   display: flex;
   justify-content: center;
@@ -44,18 +48,25 @@ const FooterMenu = () => {
     <>
       <FooterWrap>
         <FooterIcon onClick={() => navigate('/status')}>
+          <img src={내상태}></img>
+
           <FooterTitle>내 상태</FooterTitle>
         </FooterIcon>
         <FooterIcon onClick={() => navigate('/ballpenshop')}>
+          <img src={상점}></img>
+
           <FooterTitle>상점</FooterTitle>
         </FooterIcon>
         <FooterIcon onClick={() => navigate('/playpengame')}>
+          <img src={볼펜굴리기}></img>
           <FooterTitle>볼펜 굴리기</FooterTitle>
         </FooterIcon>
         <FooterIcon onClick={() => navigate('/dungeon')}>
+          <img src={던전}></img>
           <FooterTitle>던전</FooterTitle>
         </FooterIcon>
         <FooterIcon onClick={() => navigate('/ranking')}>
+          <img src={랭킹}></img>
           <FooterTitle>랭킹</FooterTitle>
         </FooterIcon>
       </FooterWrap>
