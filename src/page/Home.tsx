b90ed463cfb53c70aa04e56314b2b7f7b94b32a8
qirtudgus <qirtudgus@naver.com>
2022-09-14 04:40:03 +0900

    기존 토큰검증값을 Layout으로 이동시키면서 코드 삭제

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -1,39 +1,18 @@
-import { useSelector, useDispatch } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import { RootState } from '../modules/modules_index';
-import { logout } from '../modules/login';
 import BtnMenu from '../components/BtnMenu';
 import FooterMenu from '../components/FooterMenu';
 import React from 'react';
 import CharacterBox from '../components/CharacterBox';
 const Home = () => {
   const isTokenExPired = useSelector((state: RootState) => state.login.tokenExpired);
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
-  const logOutRequest = () => {
-    dispatch(logout());
-  };
+
   return (
     <>
       <BtnMenu LogOut></BtnMenu>
-      {isTokenExPired ? (
-        <div>
-          <p>토큰이 만료되었습니다 재로그인해주세요.</p>
-          <button
-            onClick={() => {
-              navigate('/');
-            }}
-          >
-            로그인
-          </button>
-          <button onClick={logOutRequest}>로그아웃</button>
-        </div>
-      ) : (
-        <>
-          <CharacterBox></CharacterBox>
-          <FooterMenu></FooterMenu>
-        </>
-      )}
+
+      <CharacterBox></CharacterBox>
+      <FooterMenu></FooterMenu>
     </>
   );
 };
