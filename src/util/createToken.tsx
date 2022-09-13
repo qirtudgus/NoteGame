3c2193669cb9768155a6b3c4bb32eed90b93a21b
qirtudgus <qirtudgus@naver.com>
2022-09-14 04:44:48 +0900

    토큰 생성 시 콘솔 출력

--- src/util/createToken.tsx
+++ src/util/createToken.tsx
@@ -4,6 +4,7 @@ type TokenType = string | unknown;
 
 const createToken = (id: string | any): TokenType => {
   try {
+    console.log(`${id}님의 토큰 생성`);
     return jwt.sign({ userId: id }, SECRET_TOKEN, { expiresIn: '60h' });
   } catch (err) {
     return err;
