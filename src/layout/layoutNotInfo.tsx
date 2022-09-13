7b2a3b61c41a2f8f23eca475f8a39ab459f921fb
qirtudgus <qirtudgus@naver.com>
2022-09-14 06:50:56 +0900

    layout을 쓰지않는 컴포넌트에서도 isTokenExpired로 분기

--- src/layout/layoutNotInfo.tsx
+++ src/layout/layoutNotInfo.tsx
@@ -1,10 +1,20 @@
 import { Outlet } from 'react-router-dom';
 import BackGround from '../components/BackGround';
-
+import TokenExpired from '../page/TokenExpired';
+import { useSelector } from 'react-redux';
+import { RootState } from '../modules/modules_index';
 const LayoutNotInfo = () => {
+  const isTokenExpired = useSelector((state: RootState) => state.login.tokenExpired);
+
   return (
     <BackGround>
-      <Outlet />
+      {isTokenExpired ? (
+        <TokenExpired></TokenExpired>
+      ) : (
+        <>
+          <Outlet />
+        </>
+      )}
     </BackGround>
   );
 };
