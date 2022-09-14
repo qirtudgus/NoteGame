04b940a9704b17afd0cc3df555d5e870793f5cb7
qirtudgus <qirtudgus@naver.com>
2022-09-15 06:10:25 +0900

    로그인 시 애니메이션 추가 기존에는 아이디, 비밀번호가 틀리면 alert을 띄우고 정상일경우 바로 컴포넌트를 이동했다.
    변경 후에는 store의 새로운 값을 이용하고
    loginRequest의 로직을 변경하여
    값에 따라 애니메이션을 보여준다.

--- src/App.tsx
+++ src/App.tsx
@@ -8,13 +8,41 @@ import { Link, useNavigate } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { RootState } from './modules/modules_index';
 import { login_request } from './modules/login';
+import 하얀체크 from './img/하얀체크.svg';
+import styled, { keyframes } from 'styled-components';
+
+const LoginErrorText = styled.p`
+  color: red;
+  font-size: 1.3rem;
+`;
+
+const checkAni = keyframes`
+  /* from{transition:translateY(0px)}
+  to{transition:translateY(40px)} */
+  /* from{height:0px}
+  to{height:40px} */
+  from {transform: scale(0);}
+  50% {transform: scale(1.3); }
+  to {transform: scale(0.9);}
+`;
+const Check = styled.div`
+  width: 40px;
+  height: 40px;
+  background: green;
+  border-radius: 40px;
+  animation: ${checkAni} 0.45s ease;
+`;
 
 function App() {
   const dispatch = useDispatch(); // 디스패치 함수를 가져옵니다
   const navigate = useNavigate();
   const [Id, setId] = useState<string>('');
   const [Password, setPassword] = useState<string>('');
+  // const [isLoading, setIsLoading] = useState(false);
   const isLogin = useSelector((state: RootState) => state.login.isLogin);
+  const isLoading = useSelector((state: RootState) => state.isLoadingRequest.isLoading);
+  const isLoadingText = useSelector((state: RootState) => state.isLoadingRequest.text);
+  const isLoadingCode = useSelector((state: RootState) => state.isLoadingRequest.code);
 
   const IdRef = useRef() as any;
   const PasswordRef = useRef() as any;
@@ -46,25 +74,43 @@ function App() {
     }
   }, [isLogin]);
 
+  const statusArr = [
+    null,
+    <div className='lds-ring'>
+      <div></div>
+    </div>,
+    <Check>
+      <img src={하얀체크}></img>
+    </Check>,
+    null,
+  ];
+
+  const statusTextarr = ['로그인', null, null, '로그인'];
+
   return (
     <>
       <BasicInputs
         ref={IdRef}
         value={Id}
         placeholder='아이디'
+        margin='0 0 0 0'
         OnChange={onChangeId}
       ></BasicInputs>
       <BasicInputs
         ref={PasswordRef}
         value={Password}
         placeholder='비밀번호'
+        margin='0 0 1rem 0'
         OnChange={onChangePassword}
         type='password'
       ></BasicInputs>
+      <LoginErrorText> {isLoadingText}</LoginErrorText>
       <BasicBtn
-        ButtonText='로그인'
+        ButtonText={statusTextarr[isLoadingCode]}
         OnClick={loginRequest}
-      ></BasicBtn>
+      >
+        {statusArr[isLoadingCode]}
+      </BasicBtn>
       <BasicBtn
         ButtonText='회원가입'
         OnClick={() => navigate('/register')}
