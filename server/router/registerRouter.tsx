223aa0756dd82bc6a5f4151fc568a6d1c1a94aa7
qirtudgus <qirtudgus@naver.com>
2022-09-16 16:32:51 +0900

    코드 로직 변경 1. 정규표현식 변수 지역변경 2. 닉네임 공백 체크(사용 불가)

--- server/router/registerRouter.tsx
+++ server/router/registerRouter.tsx
@@ -5,6 +5,15 @@ import { createHashPassword } from '../../src/util/createHashPassword.js';
 
 export const registerRouter = express.Router();
 
+//아이디 양식
+const idCheckRegex = /^[a-zA-Z0-9]{2,10}$/g;
+
+//특수문자 체크
+const special_pattern = /[`~!@#$%^&*|\\\'\";:\/?]/gi;
+
+//공백체크 표현식
+const spaceCheck = /\s/g;
+
 //회원가입 완료
 registerRouter.post('/join', (req: Request, res: Response, next: NextFunction) => {
   const { id, password, nickname } = req.body;
@@ -21,28 +30,20 @@ registerRouter.post('/confirmid', (req: Request, res: Response, next: NextFuncti
   const { id } = req.body;
   const idCheck = 'SELECT * FROM users WHERE ID = ?';
 
-  //아이디 양식
-  const idCheckRegex = /^[가-힣a-zA-Z0-9]{2,10}$/g;
-
-  //특수문자 체크
-  const special_pattern = /[`~!@#$%^&*|\\\'\";:\/?]/gi;
-
-  //공백체크 표현식
-  const spaceCheck = /\s/g;
   if (id === '') {
-    res.json({ auth: false, text: '2~10자 영문,한글,숫자만 사용 가능합니다.' });
+    res.json({ auth: false, text: '2~10자 영문,숫자만 사용 가능합니다.' });
     return;
   }
   if (spaceCheck.test(id)) {
-    res.json({ auth: false, text: '2~10자 영문,한글,숫자만 사용 가능합니다.' });
+    res.json({ auth: false, text: '2~10자 영문,숫자만 사용 가능합니다.' });
     return;
   }
   if (special_pattern.test(id)) {
-    res.json({ auth: false, text: '2~10자 영문,한글,숫자만 사용 가능합니다.' });
+    res.json({ auth: false, text: '2~10자 영문,숫자만 사용 가능합니다.' });
     return;
   }
   if (!idCheckRegex.test(id)) {
-    res.json({ auth: false, text: '2~10자 영문,한글,숫자만 사용 가능합니다.' });
+    res.json({ auth: false, text: '2~10자 영문,숫자만 사용 가능합니다.' });
     return;
   } else {
     //정규표현식 패스하면
@@ -63,9 +64,18 @@ registerRouter.post('/confirmnickname', (req, res, next) => {
   const { nickname } = req.body;
   const nicknameCheck = 'SELECT * FROM users WHERE Nickname = ?';
 
+  if (nickname === '') {
+    res.json({ auth: false, text: '닉네임을 입력해주세요, 공백은 사용할 수 없어요!' });
+    return;
+  }
+  if (spaceCheck.test(nickname)) {
+    res.json({ auth: false, text: '공백은 사용할 수 없어요!' });
+    return;
+  }
+
   db.query(nicknameCheck, [nickname], (err, rows, fields) => {
     if (rows[0] === undefined) {
-      console.log('사용가능한 아이디');
+      console.log('사용가능한 닉네임');
       res.status(200).json({ auth: true, text: '사용 가능한 닉네임입니다.' });
       // res.send(true);
     } else if (rows[0]) {
