52289cef33ca6fe2680e812eb182f78e7dabb455
qirtudgus <qirtudgus@naver.com>
2022-09-16 17:30:19 +0900

    이메일 검증 정규표현식 추가

--- src/util/RegExps.tsx
+++ src/util/RegExps.tsx
@@ -1,7 +1,10 @@
-//
+//공백 체크
 export const notSpaceReg = /[^0-9]/g;
 
 //공백을 ''로 변경하여 리턴해주는 함수
 export const notSpaceRegFunc = (e: any) => {
   return e.currentTarget.value.replace(notSpaceReg, '');
 };
+
+//이메일 체크
+export const emailReg = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
