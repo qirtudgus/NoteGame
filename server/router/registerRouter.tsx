2cac12c8df49d4d1fc28b99094783976afc04fad
qirtudgus <qirtudgus@naver.com>
2022-09-18 05:23:07 +0900

    코드 오류 수정 사용불가능한 값으로 중복확인을 계속 시도하면 3~4번마다 사용가능하다 뜰 때가 있었다. 정규표현식 패턴의 g 플래그를 제거하니 해결됐다.

--- server/router/registerRouter.tsx
+++ server/router/registerRouter.tsx
@@ -1,15 +1,14 @@
 import express, { Request, Response, NextFunction } from 'express';
-import CryptoJS from 'crypto-js';
 import { db } from '../db.js';
 import { createHashPassword } from '../../src/util/createHashPassword.js';
 
 export const registerRouter = express.Router();
 
 //아이디 양식
-const idCheckRegex = /^[a-zA-Z0-9]{2,10}$/g;
+const idCheckRegex = /^[a-zA-Z0-9]{2,10}$/;
 
 //특수문자 체크
-const special_pattern = /[`~!@#$%^&*|\\\'\";:\/?]/gi;
+const special_pattern = /[`~!@#$%^&*|\\\'\";:\/?]/i;
 
 //공백체크 표현식
 const spaceCheck = /\s/g;
@@ -29,9 +28,8 @@ registerRouter.post('/join', (req: Request, res: Response, next: NextFunction) =
 registerRouter.post('/confirmid', (req: Request, res: Response, next: NextFunction) => {
   const { id } = req.body;
   const idCheck = 'SELECT * FROM users WHERE ID = ?';
-
   if (id === '') {
-    res.json({ auth: false, text: '2~10자 영문,숫자만 사용 가능합니다.' });
+    res.json({ auth: false, text: '필수 정보입니다.' });
     return;
   }
   if (spaceCheck.test(id)) {
@@ -45,19 +43,19 @@ registerRouter.post('/confirmid', (req: Request, res: Response, next: NextFuncti
   if (!idCheckRegex.test(id)) {
     res.json({ auth: false, text: '2~10자 영문,숫자만 사용 가능합니다.' });
     return;
-  } else {
-    //정규표현식 패스하면
-    db.query(idCheck, [id], function (err, rows, fields) {
-      if (rows[0] === undefined) {
-        console.log('사용가능한 아이디');
-        res.status(200).json({ auth: true, text: '사용 가능한 아이디입니다.' });
-        // res.send(true);
-      } else if (rows[0]) {
-        res.status(200).json({ auth: false, text: '이미 사용중인 아이디입니다.' });
-        // res.send(false);
-      }
-    });
   }
+
+  //정규표현식 패스하면
+  db.query(idCheck, [id], function (err, rows, fields) {
+    if (rows[0] === undefined) {
+      console.log('사용가능한 아이디');
+      res.status(200).json({ auth: true, text: '사용 가능한 아이디입니다.' });
+      // res.send(true);
+    } else if (rows[0]) {
+      res.status(200).json({ auth: false, text: '이미 사용중인 아이디입니다.' });
+      // res.send(false);
+    }
+  });
 });
 
 registerRouter.post('/confirmnickname', (req, res, next) => {
@@ -65,11 +63,15 @@ registerRouter.post('/confirmnickname', (req, res, next) => {
   const nicknameCheck = 'SELECT * FROM users WHERE Nickname = ?';
 
   if (nickname === '') {
-    res.json({ auth: false, text: '닉네임을 입력해주세요, 공백은 사용할 수 없어요!' });
+    res.json({ auth: false, text: '닉네임을 입력해주세요!' });
     return;
   }
   if (spaceCheck.test(nickname)) {
-    res.json({ auth: false, text: '공백은 사용할 수 없어요!' });
+    res.json({ auth: false, text: '공백과 특수문자는 사용할 수 없어요!' });
+    return;
+  }
+  if (special_pattern.test(nickname)) {
+    res.json({ auth: false, text: '공백과 특수문자는 사용할 수 없어요!' });
     return;
   }
 
