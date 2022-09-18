55497240ef5a0b13f3a6433d0adf05ef0025faa4
qirtudgus <qirtudgus@naver.com>
2022-09-19 03:10:38 +0900

    메시지박스 기능 추가 캐릭터와 상호작용을 할 수 있는 메시지박스다. 캐릭터를 누를때마다 다른 스크립트가 출력된다.

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -1,17 +1,60 @@
-import { useSelector } from 'react-redux';
-import { RootState } from '../modules/modules_index';
 import BtnMenu from '../components/BtnMenu';
 import FooterMenu from '../components/FooterMenu';
-import React from 'react';
+import React, { useState } from 'react';
 import CharacterBox from '../components/CharacterBox';
+import styled from 'styled-components';
+import createRandomNum from '../util/createRandomNum';
+
+const MessageBox = styled.div`
+  width: auto;
+  position: relative;
+  top: 20px;
+  padding: 20px 0 20px 20px;
+  background: #fff;
+  box-shadow: 0px 7px 12px 3px rgb(0 0 0 / 30%);
+  border-radius: 10px;
+  font-size: 1.2rem;
+  &:after {
+    border-top: 10px solid #fff;
+    border-left: 10px solid transparent;
+    border-right: 10px solid transparent;
+    border-bottom: 0px solid transparent;
+    content: '';
+    position: relative;
+    top: 53px;
+    left: -78px;
+  }
+`;
+
+const CallCharacterMsg = styled.div`
+  cursor: pointer;
+`;
+
+const msgArr = [
+  '안녕하세요....',
+  '요즘은 너무 피곤해...',
+  '너 오늘 문방구 다녀왔어?',
+  '키...키사마!!',
+  '이러다 내 옆구리가 찢어지겠어!!',
+  '이러다 내 옆구리가 눅눅해지겠어....',
+  '더 이쁜 볼펜은 없어?',
+  '잉크가 다 떨어져가는거같아',
+];
+
 const Home = () => {
-  const isTokenExPired = useSelector((state: RootState) => state.login.tokenExpired);
+  const [msgRandomNumber, setMsgRandomNumber] = useState(0);
+
+  const msgRandomReturn = () => {
+    setMsgRandomNumber(createRandomNum(0, msgArr.length - 1));
+  };
 
   return (
     <>
       <BtnMenu LogOut></BtnMenu>
-
-      <CharacterBox></CharacterBox>
+      <MessageBox>{msgArr[msgRandomNumber]}</MessageBox>
+      <CallCharacterMsg onClick={msgRandomReturn}>
+        <CharacterBox></CharacterBox>
+      </CallCharacterMsg>
       <FooterMenu></FooterMenu>
     </>
   );
