f3a0e4214af58c506a51f487b33289eab37e02ed
qirtudgus <qirtudgus@naver.com>
2022-09-20 00:13:26 +0900

    ec2 아이피로 변경

--- src/util/axios.tsx
+++ src/util/axios.tsx
@@ -1,11 +1,12 @@
 import axios from 'axios';
 
-const DOMAIN = 'http://localhost:1234';
+const DOMAIN = 'http://localhost:3000';
+const DOMAIN_EC2 = 'http://54.180.46.178:3000';
 
 export const customAxios = async (method: string, url: string, data?: any): Promise<any> => {
   return await axios({
     method,
-    url: DOMAIN + url,
+    url: DOMAIN_EC2 + url,
     data,
   });
 };
