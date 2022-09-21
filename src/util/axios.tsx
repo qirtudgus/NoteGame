b6ffb09eec384295af0a9f2ce9de0c4a5f94b40a
qirtudgus <qirtudgus@naver.com>
2022-09-22 05:20:20 +0900

    TEST Build #3

--- src/util/axios.tsx
+++ src/util/axios.tsx
@@ -5,6 +5,9 @@ const DOMAIN_EC2 = 'http://54.180.46.178:3001';
 const TEST_DOMAIN = process.env.REACT_APP_SERVER_API || DOMAIN_EC2;
 
 export const customAxios = async (method: string, url: string, data?: any): Promise<any> => {
+  console.log(process.env.REACT_APP_SERVER_API);
+  console.log(DOMAIN_EC2);
+  console.log(TEST_DOMAIN);
   return await axios({
     method,
     url: TEST_DOMAIN + url,
