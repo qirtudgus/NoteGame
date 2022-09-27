259a26d4e8e0d19619c0347cc22c4239adaa7b40
qirtudgus <qirtudgus@naver.com>
2022-09-28 06:47:54 +0900

    코드 로직 추가(도움말) 리덕스의 isVisible값을 감지하여 도움말창을 띄웁니다. isVisible은 userInfo컴포넌트의 물음표버튼을 통해 핸들링됩니다.

--- src/layout/layout.tsx
+++ src/layout/layout.tsx
@@ -1,16 +1,27 @@
 import { Outlet } from 'react-router-dom';
 import BackGround from '../components/BackGround';
 import UserInfo from '../components/userInfo';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { RootState } from '../modules/modules_index';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import TokenExpired from '../page/TokenExpired';
+import HelpModal from '../components/HelpModal';
+import { userInfo_visible_off } from '../modules/userInfoVisible';
 const Layout = () => {
   const isLogin = useSelector((state: RootState) => state.login.isLogin);
   const isTokenExpired = useSelector((state: RootState) => state.login.tokenExpired);
-
+  const isVisible = useSelector((state: RootState) => state.userInfo_visibleRequest.isVisible);
+  const dispatch = useDispatch();
   return (
     <BackGround>
+      {isVisible && (
+        <HelpModal
+          close
+          OnClick={() => {
+            dispatch(userInfo_visible_off());
+          }}
+        ></HelpModal>
+      )}
       {isTokenExpired ? (
         <TokenExpired></TokenExpired>
       ) : (
