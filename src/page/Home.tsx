109b37a662782c556cde25820ca5aad2a5dc1d94
qirtudgus <qirtudgus@naver.com>
2022-09-29 05:04:37 +0900

    코드 로직 추가 (도움말팝업 여부값) -추가사항 IsHelpModal이라는 전역상태값을 기준으로 도움말팝업 렌더링여부를 결정한다. 물음표 마크는 absolute를 통해 같은 좌표에 겹치는 편법을 사용하고 화면상에 강조하기위해 반짝이는 애니메이션을 넣었다.

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -2,9 +2,15 @@ import BtnMenu from '../components/BtnMenu';
 import FooterMenu from '../components/FooterMenu';
 import React, { useEffect, useState } from 'react';
 import CharacterBox from '../components/CharacterBox';
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import createRandomNum from '../util/createRandomNum';
-
+import RevivalModal from '../components/RevivalModal';
+import BasicBtn from '../components/BasicBtn';
+import { useSelector } from 'react-redux';
+import { RootState } from '../modules/modules_index';
+import { ishelpmodal_confirm } from '../modules/login';
+import { useDispatch } from 'react-redux';
+import 물음표 from '../img/물음표_원형.svg';
 const MessageBox = styled.div`
   width: auto;
   position: relative;
@@ -44,9 +50,36 @@ const msgArr = [
   '난 연필이 좋더라',
 ];
 
+const helpHighLight = keyframes`
+  from{background:none; box-shadow:none}
+  50%{background:#fff;   box-shadow: 0px 0px 12px -1px #ffffff;}
+  to{background:none;box-shadow:none}
+`;
+
+const Help = styled.div`
+  z-index: 1000;
+  position: absolute;
+  right: -223px;
+  top: -191px;
+  margin-left: 0.6rem;
+  width: 70px;
+  height: 70px;
+  border-radius: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  & img {
+    width: 40px;
+  }
+  animation: ${helpHighLight} 0.7s infinite;
+`;
+
 const Home = () => {
   const [msgRandomNumber, setMsgRandomNumber] = useState(0);
-
+  const [firstLoginModal, setFirstLoginModal] = useState(false);
+  const IsHelpModal = useSelector((state: RootState) => state.login.userInfo?.IsHelpModal);
+  const dispatch = useDispatch();
   const msgRandomReturn = () => {
     setMsgRandomNumber(createRandomNum(0, msgArr.length - 1));
   };
@@ -55,8 +88,31 @@ const Home = () => {
     setInterval(() => msgRandomReturn(), 5000);
   }, []);
 
+  useEffect(() => {
+    if (IsHelpModal === 0) {
+      setFirstLoginModal(true);
+    } else {
+      setFirstLoginModal(false);
+    }
+  }, [IsHelpModal]);
+
   return (
     <>
+      {firstLoginModal && (
+        <RevivalModal
+          close
+          OnClick={() => setFirstLoginModal(false)}
+        >
+          <Help>
+            <img src={물음표}></img>
+          </Help>
+          처음이라면 팝업을 닫은 후 오른쪽상단의 <br></br>도움말을 읽어보시면 좋아요!
+          <BasicBtn
+            ButtonText={'팝업은 그만 볼래요..'}
+            OnClick={() => dispatch(ishelpmodal_confirm())}
+          ></BasicBtn>
+        </RevivalModal>
+      )}
       <BtnMenu LogOut></BtnMenu>
       <MessageBox>{msgArr[msgRandomNumber]}</MessageBox>
       <CallCharacterMsg onClick={msgRandomReturn}>
