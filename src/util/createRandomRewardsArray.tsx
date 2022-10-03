3959527efc909ee95591c412d32767d500ebd20d
qirtudgus <qirtudgus@naver.com>
2022-10-04 04:11:44 +0900

    UpMulilpleReward의 값에따른 분기 추가

--- src/util/createRandomRewardsArray.tsx
+++ src/util/createRandomRewardsArray.tsx
@@ -1,4 +1,4 @@
-import { rewardsListObj, rewardsList } from './rewardsList';
+import { rewardsListObj, rewardsListObj_multiple, rewardsList } from './rewardsList';
 import createRandomNumArray from './createRandomNumArray';
 import { dungeonRewardsListObj, dungeonRewardsList } from './dungeonRewardList';
 import createRandomNum from '../util/createRandomNum';
@@ -6,24 +6,45 @@ import createRandomNum from '../util/createRandomNum';
 export type useListName = 'penGame' | 'dungeon';
 export type lists = rewardsList[] & dungeonRewardsList[];
 
-export function createRandomRewardsArray(boxCount: number | null, useListName: useListName): lists {
+export function createRandomRewardsArray(
+  boxCount: number | null,
+  useListName: useListName,
+  UpMulilpleReward?: number,
+): lists {
   let rewards: lists = [];
   const randomResult = createRandomNumArray(boxCount, useListName);
 
-  switch (useListName) {
-    case 'penGame':
-      randomResult.map((i: number) => {
-        return rewards.push(rewardsListObj[i]);
-      });
-      break;
-    case 'dungeon':
-      randomResult.map((i: number) => {
-        return rewards.push(dungeonRewardsListObj[i]);
-      });
+  //멀티플스킬이 찍혀있을 경우
+  if (UpMulilpleReward === 1) {
+    switch (useListName) {
+      case 'penGame':
+        randomResult.map((i: number) => {
+          return rewards.push(rewardsListObj_multiple[i]);
+        });
+        break;
+      case 'dungeon':
+        randomResult.map((i: number) => {
+          return rewards.push(dungeonRewardsListObj[i]);
+        });
+    }
+  } else {
+    switch (useListName) {
+      case 'penGame':
+        randomResult.map((i: number) => {
+          return rewards.push(rewardsListObj[i]);
+        });
+        break;
+      case 'dungeon':
+        randomResult.map((i: number) => {
+          return rewards.push(dungeonRewardsListObj[i]);
+        });
+    }
   }
+
   return rewards;
 }
 
+//던전에서 사용하는 값
 export function createPenRewardArray(arr: number[]): number[] | undefined {
   let rewards: number[] = [];
   let rewardsCount = 6;
