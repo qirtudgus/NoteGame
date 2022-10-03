2a1a0ac11428b5379aa00d8c51c34f00819855fe
qirtudgus <qirtudgus@naver.com>
2022-10-04 04:11:55 +0900

    UpMulilpleReward로 분기되는 배열 추가

--- src/util/rewardsList.tsx
+++ src/util/rewardsList.tsx
@@ -11,7 +11,10 @@ export interface rewardsList {
   color?: string;
 }
 
-export const rewardsListObj: rewardsList[] = [
+//아래 두개의 배열은 길이가 같아야합니다.
+//랜덤한 수가 들어간 배열을 생성 시 하나의 배열로 생성하기때문
+
+export const rewardsListObj_multiple: rewardsList[] = [
   {
     front: frontText,
     number: 100,
@@ -77,6 +80,74 @@ export const rewardsListObj: rewardsList[] = [
     number: 1.5,
     back: multipleText,
     action: 'multiple',
-    color: 'red',
+  },
+];
+
+export const rewardsListObj: rewardsList[] = [
+  {
+    front: frontText,
+    number: 100,
+    back: deductText,
+    action: 'deduct',
+  },
+  {
+    front: frontText,
+    number: 100,
+    back: deductText,
+    action: 'deduct',
+  },
+  {
+    front: frontText,
+    number: 200,
+    back: deductText,
+    action: 'deduct',
+  },
+  {
+    front: frontText,
+    number: 100,
+    back: addText,
+    action: 'add',
+  },
+  {
+    front: frontText,
+    number: 200,
+    back: addText,
+    action: 'add',
+  },
+  {
+    front: frontText,
+    number: 500,
+    back: addText,
+    action: 'add',
+  },
+  {
+    front: frontText,
+    number: 1000,
+    back: addText,
+    action: 'add',
+  },
+  {
+    front: frontText,
+    number: 1000,
+    back: addText,
+    action: 'add',
+  },
+  {
+    front: frontText,
+    number: 1000,
+    back: addText,
+    action: 'add',
+  },
+  {
+    front: frontText,
+    number: 1500,
+    back: addText,
+    action: 'add',
+  },
+  {
+    front: frontText,
+    number: 2000,
+    back: addText,
+    action: 'add',
   },
 ];
