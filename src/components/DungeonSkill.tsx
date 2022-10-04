6feca653674277c2d30b10bcae2849451ecca207
qirtudgus <qirtudgus@naver.com>
2022-10-05 04:05:54 +0900

    던전 스킬아이콘 추가

--- src/components/DungeonSkill.tsx
+++ src/components/DungeonSkill.tsx
@@ -1,8 +1,9 @@
-import styled, { css } from 'styled-components';
+import styled, { css, keyframes } from 'styled-components';
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { RootState } from '../modules/modules_index';
 import { LoginUserInfoInterface } from '../modules/login';
+import 더블어택 from '../img/skill/더블어택.jpg';
 const Wrap = styled.div`
   position: absolute;
   right: 30px;
@@ -23,15 +24,50 @@ interface useSkill {
   useDoubleAttack?: boolean;
 }
 
+const helpHighLight = keyframes`
+  from{background:none; box-shadow:none}
+  50%{background:rgba(255,255,255,0.8);   box-shadow: 0px 0px 12px -1px #ffffff;}
+  to{background:none;box-shadow:none}
+`;
+
 const Icon = styled.div<useSkill>`
+  cursor: pointer;
   width: 50px;
   height: 50px;
   background: #eee;
   margin: 4px;
+  position: relative;
+
+  & img {
+    position: relative;
+    z-index: 1;
+  }
+  &::before {
+    content: '';
+    position: absolute;
+    z-index: 2;
+    top: 0px;
+    left: 0px;
+    width: 50px;
+    height: 50px;
+    background-color: rgba(0, 0, 0, 0.3);
+  }
+
   ${(props) =>
     props.useDoubleAttack &&
     css`
-      background: #e5005a;
+      outline: 2px solid#ffbc26;
+      &::before {
+        content: '';
+        position: absolute;
+        z-index: 2;
+        top: 0px;
+        left: 0px;
+        width: 50px;
+        height: 50px;
+        background-color: #e5005a;
+        animation: ${helpHighLight} 0.8s infinite;
+      }
     `}
 `;
 
@@ -46,7 +82,10 @@ const DungeonSKill = (props: any) => {
             useDoubleAttack={props.useDoubleAttack}
             onClick={props.OnClick}
           >
-            더블어택
+            <img
+              src={더블어택}
+              alt='더블어택 아이콘'
+            ></img>
           </Icon>
         ) : (
           <Icon></Icon>
