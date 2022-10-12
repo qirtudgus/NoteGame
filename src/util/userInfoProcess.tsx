51fb45603e78ee6e1f3ebcb3709a1ecaf76a3a28
qirtudgus <qirtudgus@naver.com>
2022-10-13 06:43:44 +0900

    안쓰는 컬럼값 삭제

--- src/util/userInfoProcess.tsx
+++ src/util/userInfoProcess.tsx
@@ -11,13 +11,9 @@ export interface userInfoInterface {
   BasicHp: number;
   WeaponDamage: number;
   WeaponHp: number;
-  EquipDamage: number;
-  EquipHp: number;
   beforeGold?: number;
   Gold: number;
-  Inventory: null | number;
   PenCount: number;
-  UpGoldAll: number;
   UpGoldHunt: number;
   UpGoldPen: number;
   UpMaxHp: number;
@@ -25,7 +21,6 @@ export interface userInfoInterface {
   UpWeaponDamage: number;
   UpWeaponHp: number;
   DoubleAttack: number;
-  Grabber: number;
   Exp: number;
   DungeonFloor: number;
   BetterPen: number;
