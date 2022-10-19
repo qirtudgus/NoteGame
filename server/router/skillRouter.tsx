adfd3e6dd5459649f6faba7a5537ec414552fa1e
qirtudgus <qirtudgus@naver.com>
2022-10-19 22:19:45 +0900

    코드 로직 변경(컬럼명 변수사용으로 코드 최적화)

--- server/router/skillRouter.tsx
+++ server/router/skillRouter.tsx
@@ -4,12 +4,7 @@ import { userInfoProcess } from '../../src/util/userInfoProcess.js';
 
 export const skillRouter = express.Router();
 
-const UpMoreFloorQuery = `UPDATE users SET UpMoreFloor = UpMoreFloor + 1, SkillPoint = SkillPoint - 1 WHERE ID = ?`;
-const UpRevivalStatPointQuery = `UPDATE users SET UpRevivalStatPoint = UpRevivalStatPoint + 1, SkillPoint = SkillPoint - 1 WHERE ID = ?`;
-const UpDoubleAttackQuery = `UPDATE users SET UpDoubleAttack = UpDoubleAttack + 1, SkillPoint = SkillPoint - 1 WHERE ID = ?`;
-const UpMulilpleRewardQuery = `UPDATE users SET UpMulilpleReward = UpMulilpleReward + 1, SkillPoint = SkillPoint - 1 WHERE ID = ?`;
-const RevivalPointQuery = `UPDATE users SET RevivalPoint = RevivalPoint + 1, SkillPoint = SkillPoint - 1 WHERE ID = ?`;
-
+const SkillQuery = `UPDATE users SET ?? = ?? + 1, SkillPoint = SkillPoint - 1 WHERE ID = ?`;
 const loginQuery = 'SELECT * FROM users WHERE ID = ?';
 
 skillRouter.post('/skillup', (req, res, next) => {
@@ -18,32 +13,9 @@ skillRouter.post('/skillup', (req, res, next) => {
 
   if (skillPoint <= 0) return;
   console.log(`${userId}님이 ${skillName}스킬을 찍으셨습니다.`);
-  if (skillName === 'UpMoreFloor') {
-    db.query(UpMoreFloorQuery, [userId], (err, result, fields) => {
-      // console.log(err);
-    });
-  }
-  if (skillName === 'UpRevivalStatPoint') {
-    db.query(UpRevivalStatPointQuery, [userId], (err, result, fields) => {
-      // console.log(err);
-    });
-  }
-  if (skillName === 'UpDoubleAttack') {
-    db.query(UpDoubleAttackQuery, [userId], (err, result, fields) => {
-      // console.log(err);
-    });
-  }
-  if (skillName === 'RevivalPoint') {
-    db.query(RevivalPointQuery, [userId], (err, result, fields) => {
-      // console.log(err);
-    });
-  }
-
-  if (skillName === 'UpMulilpleReward') {
-    db.query(UpMulilpleRewardQuery, [userId], (err, result, fields) => {
-      // console.log(err);
-    });
-  }
+  db.query(SkillQuery, [skillName, skillName, userId], (err, result) => {
+    if (err) console.log(err);
+  });
 
   db.query(loginQuery, [userId], (err, rows, fields) => {
     const uesrInfo = userInfoProcess(rows[0]);
