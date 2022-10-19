f03b8a104bc66d33727a89143f00bbab659d4f3f
qirtudgus <qirtudgus@naver.com>
2022-10-19 11:42:39 +0900

    코드 로직 변경(몬스터 객체 생성) 원래 img만 들어있던 배열을 name과 img로 구성된 객체가 들어있는 배열로 변경 추후 몬스터넘버나, 몬스터별 능력치도 추가할 예정 몬스터넘버는 mysql table에 값을 핸들링할때 쓴다.

--- src/util/dungeonMonsterList.tsx
+++ src/util/dungeonMonsterList.tsx
@@ -5,61 +5,139 @@ import 말미잘_1 from '../img/monster/말미잘_1.png';
 import 지렁용_1 from '../img/monster/지렁용_1.png';
 import 지렁용_2 from '../img/monster/지렁용_2.png';
 import 지렁용_3 from '../img/monster/지렁용_3.png';
-
-export const monsterArr: JSX.Element[] = [
-  <img
-    src={슬라임_1}
-    alt='monster'
-    title='몬스터'
-  ></img>,
-  <img
-    src={슬라임_2}
-    alt='monster'
-    title='몬스터'
-  ></img>,
-  <img
-    src={슬라임_3}
-    alt='monster'
-    title='몬스터'
-  ></img>,
-  <img
-    src={말미잘_1}
-    alt='monster'
-    title='몬스터'
-  ></img>,
-  <img
-    src={지렁용_1}
-    alt='monster'
-    title='몬스터'
-  ></img>,
-  <img
-    src={지렁용_2}
-    alt='monster'
-    title='몬스터'
-  ></img>,
-  <img
-    src={지렁용_3}
-    alt='monster'
-    title='몬스터'
-  ></img>,
-  // <img
-  //   src={강아지}
-  //   alt='monster'
-  //   title='몬스터'
-  // ></img>,
-  // <img
-  //   src={고양이}
-  //   alt='monster'
-  //   title='몬스터'
-  // ></img>,
-  // <img
-  //   src={돼지}
-  //   alt='monster'
-  //   title='몬스터'
-  // ></img>,
-  // <img
-  //   src={리자드}
-  //   alt='monster'
-  //   title='몬스터'
-  // ></img>,
+interface a {
+  name: string;
+  img: JSX.Element;
+}
+export const monsterArr: a[] = [
+  {
+    name: '작은 슬라임',
+    img: (
+      <img
+        src={슬라임_1}
+        alt='monster'
+        title='몬스터'
+      ></img>
+    ),
+  },
+  {
+    name: '중간 슬라임',
+    img: (
+      <img
+        src={슬라임_2}
+        alt='monster'
+        title='몬스터'
+      ></img>
+    ),
+  },
+  {
+    name: '킹 슬라임',
+    img: (
+      <img
+        src={슬라임_3}
+        alt='monster'
+        title='몬스터'
+      ></img>
+    ),
+  },
+  {
+    name: '말미잘',
+    img: (
+      <img
+        src={말미잘_1}
+        alt='monster'
+        title='몬스터'
+      ></img>
+    ),
+  },
+  {
+    name: '작은 지렁용',
+    img: (
+      <img
+        src={지렁용_1}
+        alt='monster'
+        title='몬스터'
+      ></img>
+    ),
+  },
+  {
+    name: '중간 지렁용',
+    img: (
+      <img
+        src={지렁용_2}
+        alt='monster'
+        title='몬스터'
+      ></img>
+    ),
+  },
+  {
+    name: '큰 지렁용',
+    img: (
+      <img
+        src={지렁용_3}
+        alt='monster'
+        title='몬스터'
+      ></img>
+    ),
+  },
+  {
+    name: '큰 지렁용',
+    img: (
+      <img
+        src={지렁용_3}
+        alt='monster'
+        title='몬스터'
+      ></img>
+    ),
+  },
+  {
+    name: '큰 지렁용',
+    img: (
+      <img
+        src={지렁용_3}
+        alt='monster'
+        title='몬스터'
+      ></img>
+    ),
+  },
+  {
+    name: '큰 지렁용',
+    img: (
+      <img
+        src={지렁용_3}
+        alt='monster'
+        title='몬스터'
+      ></img>
+    ),
+  },
+  {
+    name: '큰 지렁용',
+    img: (
+      <img
+        src={지렁용_3}
+        alt='monster'
+        title='몬스터'
+      ></img>
+    ),
+  },
+  {
+    name: '큰 지렁용',
+    img: (
+      <img
+        src={지렁용_3}
+        alt='monster'
+        title='몬스터'
+      ></img>
+    ),
+  },
+  {
+    name: '큰 지렁용',
+    img: (
+      <img
+        src={지렁용_3}
+        alt='monster'
+        title='몬스터'
+      ></img>
+    ),
+  },
 ];
